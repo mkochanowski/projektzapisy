c148ac149be49fd2678a02cbc0ea87a87238417e
Jan Filipowski <yashke@yashke-laptop.(none)>
2010-03-30 12:18:36 +0200

    stubbed time in news.tests

--- news/tests.py
+++ news/tests.py
@@ -5,6 +5,11 @@ from news.models import News
 
 import datetime
 
+import mox
+
+def today():
+    return datetime.date(2010, 3, 26)
+
 class NewsModelTest(TestCase):
     def test_creating_news(self):
         n1 = News(title="news test",
@@ -16,4 +21,4 @@ class NewsManagerTest(TestCase):
     
     def test_count_new(self):
         # 4 - prawidłowa wartość z fikstury
-        self.assertEquals(4, News.objects.count_new())
+        self.assertEquals(4, News.objects.count_new(now=today))
