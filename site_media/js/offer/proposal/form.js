35e6a6dc4fe9bcbc8b38a91d4db8fd1781ee68b8
achudzinski <adi@world.pl>
2010-05-11 22:13:52 +0200

    Zmiany w propozyjcach

--- site_media/js/offer/proposal/form.js
+++ site_media/js/offer/proposal/form.js
@@ -10,13 +10,13 @@ function addBook()
     var books = $('.book');
     
     var newBook = $(
-        '<tr class="book"> ' +
-        '   <td class="label" ></td>' + 
-        '   <td class="field"><input type="text" name="books[]" /></td>' + 
-        '</tr>'
+        '<div class="book"> ' +
+        '   <div class="label" >&nbsp;</div>' + 
+        '   <div class="field"><input type="text" name="books[]" /></div>' + 
+        '</div>'
     );
     
-    var lastBook = books.eq(books.length-1);
+    var lastBook = books.eq(books.length-2);
     lastBook.after(newBook);
         
     return false;
