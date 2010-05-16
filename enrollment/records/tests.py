bd3df847fa53e7889822d52e399a1d900ed651d0
Piotr Walkowski <walu911@hotmail.com>
2010-05-16 17:50:25 +0200

    Enrollment.records test fix - FINISHED

--- enrollment/records/tests.py
+++ enrollment/records/tests.py
@@ -109,7 +109,7 @@ class IsStudentInSubjectGroupTypeTest(TestCase):
     def setUp(self):
         self.user = User.objects.get(id=5)
         self.group = Group.objects.get(id=1)
-        self.group2 = Group.objects.get(id=2)
+        self.group2 = Group.objects.get(id=3)
         self.subject = Subject.objects.get(id=1)
         self.record = Record.add_student_to_group(self.user.id, self.group.id)
     
@@ -146,7 +146,7 @@ class GetStudentsInGroupTest(TestCase):
     fixtures =  ['fixtures__users', 'fixtures__subjects']
     
     def setUp(self):
-        self.user = User.objects.get(id=1)
+        self.user = User.objects.get(id=5)
         self.group = Group.objects.get(id=1)
         self.record = Record.add_student_to_group(self.user.id, self.group.id)
     
