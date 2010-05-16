7d0ea07e97dce48e41d5c231dddbe5543087b088
Marta Anna Ziobro <gyarill@gmail.com>
2010-05-16 22:09:15 +0200

    <a href="/p/fereol/issues/82/#ic260">Issue 82</a> Changed click actions of links in subject details, added loader to schedule prototype and subject list

--- enrollment/subjects/models/subject.py
+++ enrollment/subjects/models/subject.py
@@ -5,8 +5,8 @@ from django.db import models
 class SubjectEntity(models.Model):
     name = models.CharField(max_length=100)
     class Meta:
-        verbose_name = 'encja przedmiotu'
-        verbose_name_plural = 'encje przedmiotów'
+        verbose_name = 'Podstawa przedmiotu'
+        verbose_name_plural = 'Podstawy przedmiotów'
         app_label = 'subjects'
         
     def __unicode__(self):
