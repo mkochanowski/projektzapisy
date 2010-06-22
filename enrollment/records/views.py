b4d5edcfd41712c65e26c55e4594f8df10cda02e
Jan Filipowski <jachuf@gmail.com>
2010-06-22 14:12:00 +0200

    performance improved?

--- enrollment/records/views.py
+++ enrollment/records/views.py
@@ -244,15 +244,14 @@ def own(request):
 def schedulePrototype(request):
     try:
         student_records = Record.get_student_all_detiled_records(request.user.id)
-        #subjects = Subject.objects.all()
-        all_terms = Term.objects.select_related(depth = 2).all()
-        group_with_subjects = Group.objects.select_related(depth = 2).all()
-        subjects = set([g.subject for g in group_with_subjects])
+        subjects = Subject.objects.select_related(depth=2).all()
+        #all_terms = Term.objects.select_related(depth = 2).all()
+        #group_with_subjects = Group.objects.select_related(depth = 2).all()
+        #subjects = set([g.subject for g in group_with_subjects])
         
-        for subject in subjects:
-            subject.groups_ = group_with_subjects.filter(subject = subject)
-            for group in subject.groups_:
-                group.terms_ = all_terms.filter(group = group)
+#       for subject in subjects:
+#           for group in subject.groups_:
+#               group.terms_ = all_terms.filter(group = group)
         data = {
             'student_records': student_records,
             'subjects': subjects,
