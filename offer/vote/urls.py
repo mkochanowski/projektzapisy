c5c97edf17bf49424f81980fcb4a1d659cf6733d
Neithan <neithan@neithan-toshiba.(none)>
2010-06-23 15:10:42 +0200

    Fixed Issue 183: Podsumowanie Głosowania

--- offer/vote/urls.py
+++ offer/vote/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns( 'fereol.offer.vote.views',
     url('^$',     'vote_main', name='vote-main' ),
     url('view/$', 'vote_view', name='vote-view'),
-    url('vote/$', 'vote',      name='vote')
+    url('vote/$', 'vote',      name='vote'),
+    url('summary/$', 'vote_summary', name='vote-summary'),
+    url('summary/(?P<slug>[\w\-_]+)/$', 'proposal_vote_summary', name='proposal-vote-summary')
 )
