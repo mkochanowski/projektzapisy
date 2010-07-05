1b36f5afb7fcedfd68dfd8684db1582a0bc706f7
Jan Filipowski <jachuf@gmail.com>
2010-07-05 14:52:23 +0200

    tries

--- site_media/js/enrollment/subjects/subjects-list.js
+++ site_media/js/enrollment/subjects/subjects-list.js
@@ -39,7 +39,6 @@ function loadSubjectDetails(detailsUrl, obj){
 			try {
 				//sessionStorage.setItem('loaded-subject-detail-url', detailsUrl);
         if (obj == null || !obj.hasClass("forget")) {
-          alert(obj.attr("class"));
           jaaulde.utils.cookies.set('loaded-subject-detail-url', detailsUrl);
         }
 			} catch(ex) {}
