3d10a708f44070939021ae14ffc88984dd95f327
Maciek Jabłoński <iks@iks-Vostro-3700.(none)>
2010-12-28 17:02:58 +0100

    Tworzenie ankiet cz.1

---
+++ grade/poll/models/section.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Polls
+"""
+
+from django.db import models
+
+from fereol.grade.poll.models.poll import Poll
+
+
+class Section( models.Model ):
+    title   = models.CharField(  max_length   = 250,
+                                 verbose_name = "tytuł" )
+    poll    = models.ForeignKey ( Poll, verbose_name = 'ankieta' )
+
+    class Meta:
+        verbose_name    = "sekcja ankiety"
+        verbose_name    = "sekcje ankiety"
+        app_label       = "poll"
+
