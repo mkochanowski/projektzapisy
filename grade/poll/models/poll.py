468ef3758050857eb70e121c571304f5bbfb29c7
Maciek Jabłoński <iks@iks-Vostro-3700.(none)>
2010-12-28 17:53:30 +0100

    Tworzenie ankiet

--- grade/poll/models/poll.py
+++ grade/poll/models/poll.py
@@ -6,9 +6,6 @@
 
 from django.db import models
 
-from grade.poll.models.questions import OpenQuestion
-from grade.poll.models.questions import SingleChoiceQuestion 
-from grade.poll.models.questions import MultipleChoiceQuestion
 
 from users.models               import Employee
 from enrollment.subjects.models import Subject
@@ -19,22 +16,9 @@ class Poll( models.Model ):
                                  verbose_name = "tytuł" )
     author  = models.ForeignKey( Employee,
                                  verbose_name = "autor" )
-    subject = models.ForeignKey( Subject,
-                                 verbose_name = "przedmiot" )
-    group   = models.ForeignKey( Group,
-                                 verbose_name = "grupa" )
-    
-    single_choice_questions = models.ManyToManyField( 
-                          SingleChoiceQuestion,
-                          verbose_name = "pytania jednokrotnego wyboru")
-    multiple_choice_question = models.ManyToManyField(
-                          MultipleChoiceQuestion,
-                          verbose_name = "pytania wielokrotnego wyboru")
-    open_questions           = models.ManyToManyField(
-                          OpenQuestion,
-                          verbose_name = "pytania otwarte")
-                          
+                         
     class Meta:
         verbose_name        = "ankieta"
         verbose_name_plural = "ankiety"
         app_label           = "poll"
+
