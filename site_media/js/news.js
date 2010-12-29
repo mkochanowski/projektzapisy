294ffdc59c5dae7b89b1eee5e9f4779e8fad6af3
Tomek Wasilczyk <tomkiewicz@o2.pl>
2010-12-29 12:00:21 +0100

    Poprawki html i js w module newsow

--- site_media/js/news.js
+++ site_media/js/news.js
@@ -1,33 +1,45 @@
-function load_data(url) {
-    MessageBox.clear();
-    $.getJSON(url, function(data) {
-	if (typeof(data.message) != "undefined") {
-        MessageBox.display(data.message);
-	} else {
-	    $('#news-nav-groups').html(data.newer_group + data.older_group);
-	    $('#news-content').html(data.content);
-	    if (data.archive_view) {
-		$('#news-archive-nav').css("display", "inline");
-	    } else {
-		$('#news-archive-nav').css("display", "none");
-	    }
-	    if (data.search_view) {
-		$('#news-search-nav').css("display", "inline");
-	    } else {
-		$('#news-search-nav').css("display", "none");
-	    }
-	}
-    });
+/**
+ * Funkcje i klasy odpowiedzialne za moduł newsów.
+ */
+
+News = Object();
+
+News.init = function()
+{
+	var category = $('#od-news-search-form').attr('action').split('/')[2];
+	$('#od-news-search-form').submit(function()
+	{
+		News.loadPage('/news/' + category + '/search/?json=true&' +
+			'q=' + $('#od-news-search-q').attr('value'));
+		return false;
+	});
+	$('#od-news-search-reset').click(function()
+	{
+		News.loadPage('/news/' + category + '/?json=true');
+	});
 };
 
-$(document).ready(function() {
-    var category = $('#od-news-search-form').attr('action').split('/')[2]
-    $('#od-news-search-form').submit(function() {
-	load_data('/news/' + category + '/search/?json=true&' +
-		  'q=' + $('#od-news-search-q').attr('value'));
-	return false;
+$(News.init);
+
+News.loadPage = function(url)
+{
+    MessageBox.clear();
+    $.getJSON(url, function(data)
+	{
+		if (typeof(data.message) != "undefined")
+			MessageBox.display(data.message);
+		else
+		{
+			$('#news-nav-groups').html(data.newer_group + data.older_group);
+			$('#news-content').html(data.content);
+			if (data.archive_view)
+				$('#news-archive-nav').css("display", "inline");
+			else
+				$('#news-archive-nav').css("display", "none");
+			if (data.search_view)
+				$('#news-search-nav').css("display", "inline");
+			else
+				$('#news-search-nav').css("display", "none");
+		}
     });
-    $('#od-news-search-reset').click(function() {
-	load_data('/news/' + category + '/?json=true');
-    });
-});
+};
