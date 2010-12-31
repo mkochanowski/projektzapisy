31a80101bd20e701ce8e71943cf44a97dd9eb06f
Tomek Wasilczyk <tomkiewicz@o2.pl>
2010-12-31 04:33:21 +0100

    Issue 171: [OD] preferencje - widok listy - drobny błąd w komentarzu

--- offer/preferences/urls.py
+++ offer/preferences/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-# to tree_view and description, pass by GET:
+# to view and description, pass by GET:
 # format=json - returns data as json
 # format=html - returns data rendered with template, not using the base template
 # by default, they return a fully rendered page
