fb1e534e7fb9ab4fdf1f4a5b3157d52175dd83ff
Maciek Jabłoński <iks@iks-Vostro-3700.(none)>
2011-01-15 00:34:26 +0100

    Tworzenie ankiet - dynamiczne wczytywanie parametrow

--- grade/poll/urls.py
+++ grade/poll/urls.py
@@ -4,7 +4,9 @@ urlpatterns = patterns('',
     url(r'poll_create$', 'fereol.grade.poll.views.poll_create', name='grade-poll-poll-create'),
     url(r'questionset_create$', 'fereol.grade.poll.views.questionset_create', name='grade-poll-questionset-create'),
     url(r'questionset_assign$', 'fereol.grade.poll.views.questionset_assign', name='grade-poll-questionset-assign'),
-        
+    url(r'ajax_get_subjects$',  'fereol.grade.poll.views.ajax_get_subjects',  name='grade-poll-get-subjects'),
+    url(r'ajax_get_groups$',    'fereol.grade.poll.views.ajax_get_groups',    name='grade-poll-get-groups'),
+
     url(r'tickets_enter$', 'fereol.grade.poll.views.tickets_enter', name='grade-poll-tickets-enter'),
     url(r'poll_answer$', 'fereol.grade.poll.views.poll_answer', name='grade-poll-questionset-poll-answer'),
     url(r'poll_save$', 'fereol.grade.poll.views.poll_save', name='grade-poll-poll-save'),
--- site_media/js/grade/poll/create.js
+++ site_media/js/grade/poll/create.js
@@ -8,16 +8,108 @@ Poll.create.init = function()
     Poll.create.sections      = $('#sections');
     Poll.create.chosenSection = $('#sections-list').children('ul')[0];
 
-    $('#section-add').click(function()
-    {
-        Poll.create.addSection();
-    });
+    $('#section-add').click(Poll.create.addSection);
+    $('#semester').change(Poll.create.changeSemester);
+    $('#subjects').change(Poll.create.changeSubjects);
+    $('#type').change(Poll.create.changeTypes);
+
+    $('.type-visibility').hide();
+    $('.group-visibility').hide();
 
     $(Poll.create.chosenSection).sortable({handle : 'p'});
 }
 
 $(Poll.create.init);
 
+Poll.create.changeSubjects = function()
+{
+    var subject = $('#subjects').val();
+    if ( subject > 0 )
+    {
+        $('.type-visibility').show();
+        Poll.create.changeTypes();
+    }
+    else if (subject == 0)
+    {
+        $('.type-visibility').show();
+        Poll.create.changeTypes();
+    }
+    else
+    {
+        $('.type-visibility').hide();
+        $('.group-visibility').hide();
+    }
+}
+
+Poll.create.changeTypes = function()
+{
+    var type    = $('#type').val();
+    var subject = $('#subjects').val();
+    if ( type > 0 && subject > 0 )
+    {
+        $('.group-visibility').show();
+        dataString = 'type=' + type + '&subject=' + subject
+        $.ajax({
+            type: "POST",
+            url: "grade/poll/ajax_get_groups",
+            async: false,
+            dataType: 'json',
+            data: dataString,
+            success: Poll.create.loadGroups
+        });
+    }
+    if (type < 1)
+    {
+        $('.group-visibility').hide();
+        
+    }
+}
+
+Poll.create.loadGroups  = function(groups)
+{
+    $("#group").children().remove();
+    $("#group").append( Poll.create.createOption('0', 'Wszystkie grupy') );
+    $.each(groups, function(key, group)
+    {
+        $("#group").append( Poll.create.createOption( group[0], group[1] ) );
+    });
+}
+
+Poll.create.changeSemester = function()
+{
+    var semester = $('#semester').val();
+    dataString = 'semester=' + semester
+    $.ajax({
+        type: "POST",
+        url: "grade/poll/ajax_get_subjects",
+        async: false,
+        dataType: 'json',
+        data: dataString,
+        success: Poll.create.loadSubjects
+    });
+}
+
+Poll.create.loadSubjects = function(subjects)
+{
+    $("#subjects").children().remove();
+    var item1 = Poll.create.createOption('-1', 'Nie przypisane do przedmiotu')
+    var item2 = Poll.create.createOption('0', 'Wszystkie grupy')
+    $("#subjects").append(item1);
+    $("#subjects").append(item2);
+    $.each(subjects, function(key, value)
+    {
+        $("#subjects").append( Poll.create.createOption( value[0], value[1] ) );
+    });
+
+}
+
+Poll.create.createOption = function(value, text)
+{
+    var item = document.createElement('option');
+    item.value = value
+    $(item).text(text)
+    return item
+}
 Poll.create.initSection = function(sectionElement)
 {
 
