a436edf6a38891c2ce28bbf136b8b4dda20dbc41
Tomek Wasilczyk <tomkiewicz@o2.pl>
2011-01-23 15:40:53 +0100

    Subject type: removing uniqueness constraint

--- enrollment/subjects/models/subject_type.py
+++ enrollment/subjects/models/subject_type.py
@@ -5,9 +5,10 @@ from django.db import models
      
 class Type(models.Model):
     """types of subjects"""
-    name = models.CharField(max_length=30, verbose_name='rodzaj zajec', default="", unique=True)
+    name = models.CharField(max_length=30, verbose_name='rodzaj zajec', default="", unique=False)
     group     = models.ForeignKey("self", null=True, blank=True, verbose_name='grupa')
     meta_type = models.BooleanField(default = False, verbose_name ='Grupa typow')
+	#TODO: dodać unique na parę (meta_type, name)
     
     @staticmethod
     def get_all_types():
