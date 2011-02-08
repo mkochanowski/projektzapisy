3e57d3b94c5dd571c7e7ba11e08b5fd7fa46794a
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-02-08 16:16:25 +0100

    Added capacity and building field

--- enrollment/subjects/models/classroom.py
+++ enrollment/subjects/models/classroom.py
@@ -5,6 +5,8 @@ from django.db import models
 class Classroom( models.Model ):
     """classroom in institute"""
     number = models.CharField( max_length = 4, verbose_name = 'numer sali' )
+    building = models.CharField( max_length = 50, verbose_name = 'budynek', blank=True, default='' )
+    capacity = models.PositiveSmallIntegerField(default=0, verbose_name='liczba miejsc')   
     
     class Meta:
         verbose_name = 'sala'
