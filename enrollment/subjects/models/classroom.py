16abe2cf5f8a752c46b90e547b40a8f859be2c06
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-02-09 22:15:30 +0100

    Pole budynek powiększone

--- enrollment/subjects/models/classroom.py
+++ enrollment/subjects/models/classroom.py
@@ -5,7 +5,7 @@ from django.db import models
 class Classroom( models.Model ):
     """classroom in institute"""
     number = models.CharField( max_length = 4, verbose_name = 'numer sali' )
-    building = models.CharField( max_length = 50, verbose_name = 'budynek', blank=True, default='' )
+    building = models.CharField( max_length = 75, verbose_name = 'budynek', blank=True, default='' )
     capacity = models.PositiveSmallIntegerField(default=0, verbose_name='liczba miejsc')   
     
     class Meta:
