4c96ef5d0301ca91a782bd866df8ede21516b64f
Jakub Tarnawski <dj3500@gmail.com>
2011-02-15 15:32:47 +0100

    Adding configuration to env.rb to allow for end2end testing in Chrome and IE.

--- tests/end2end/features/support/env.rb
+++ tests/end2end/features/support/env.rb
@@ -4,5 +4,24 @@ require 'selenium-webdriver'
 require 'capybara/cucumber'
 
 Capybara.default_driver = :selenium
-#Capybara.app_host = "http://nowe-zapisy.ii.uni.wroc.pl"
 Capybara.app_host = "http://localhost:8000"
+Capybara.default_wait_time = 5 # we wait for 5 seconds until failing a test
+
+Capybara.register_driver :selenium_remote_ie do |app|
+   Capybara::Driver::Selenium.new(app, :browser => :remote, :desired_capabilities => :internet_explorer,
+                                       :url => "http://fereol-ci.ii.uni.wroc.pl:4044/wd/hub")
+end
+
+Capybara.register_driver :selenium_local_chrome do |app|
+   Capybara::Driver::Selenium.new(app, :browser => :chrome)
+end
+
+if ENV['BROWSER'] == "ie"
+   Capybara.default_driver = :selenium_remote_ie
+   Capybara.app_host = "http://10.0.2.2:4026"
+   # w tym przypadku trzeba odpalać gunicorn_django z parametrem 0.0.0.0:8000, aby aplikacja przyjmowała żądania spoza localhosta
+elsif ENV['BROWSER'] == "chrome"
+   Capybara.default_driver = :selenium_local_chrome
+else
+   Capybara.default_driver = :selenium
+end
