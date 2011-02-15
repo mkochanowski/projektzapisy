2411968ae9290e4d0703cbbdeffc9b8e732b8277
Jakub Tarnawski <dj3500@gmail.com>
2011-02-15 15:40:31 +0100

    Just corrected some comments

--- tests/end2end/features/support/env.rb
+++ tests/end2end/features/support/env.rb
@@ -9,7 +9,7 @@ Capybara.default_wait_time = 5 # we wait for 5 seconds until failing a test
 
 Capybara.register_driver :selenium_remote_ie do |app|
    Capybara::Driver::Selenium.new(app, :browser => :remote, :desired_capabilities => :internet_explorer,
-                                       :url => "http://fereol-ci.ii.uni.wroc.pl:4044/wd/hub")
+                                       :url => "http://10.0.2.2:4044/wd/hub") # 10.0.2.2 to jest host fereol-ci, z punktu widzenia Virtual-Ubuntu
 end
 
 Capybara.register_driver :selenium_local_chrome do |app|
@@ -18,8 +18,8 @@ end
 
 if ENV['BROWSER'] == "ie"
    Capybara.default_driver = :selenium_remote_ie
-   Capybara.app_host = "http://10.0.2.2:4026"
-   # w tym przypadku trzeba odpalać gunicorn_django z parametrem 0.0.0.0:8000, aby aplikacja przyjmowała żądania spoza localhosta
+   Capybara.app_host = "http://10.0.2.2:4026" # 10.0.2.2 to jest host fereol-ci, z punktu widzenia maszyny Virtual-XP
+   # w tym przypadku trzeba odpalać gunicorn_django z parametrem -b 0.0.0.0:8000, aby aplikacja przyjmowała żądania spoza localhosta
 elsif ENV['BROWSER'] == "chrome"
    Capybara.default_driver = :selenium_local_chrome
 else
