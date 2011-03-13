107f294964effd99d7965a9b8e3d87c2276bd004
Maciek Jabłoński <iks@iks-Vostro-3700.(none)>
2011-03-13 23:57:54 +0100

    Teraz przechodzi już testy

--- apps/news/tests/tags.py
+++ apps/news/tests/tags.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 from utils import generate_random_news
 
-from news.templatetags.news_extras import *
+from apps.news.templatetags.news_extras import *
 
 class NewsTagsTest(TestCase):
     def test_newscount(self):
