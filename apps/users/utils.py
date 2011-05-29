15b7c085af58cfbadc7d749904c7d4e7ba4a80d1
Tomek Wasilczyk <tomkiewicz@o2.pl>
2011-05-29 19:07:40 +0200

    Invalid ids in student and employee profiles (DYNAMIC TYPING FOR THE WIN)

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -4,19 +4,19 @@ from django.core.urlresolvers import reverse
 
 def prepare_ajax_students_list(students):
     return map(lambda s:
-           {'id':       s.id,
+           {'id':       s.user.id,
             'album':    s.matricula,
             'recorded': s.recorded,
             'email':    s.user.email,
             'name':     u'%s %s' % (s.user.first_name, s.user.last_name),
-            'link':     reverse('student-profile', args=[s.id])}, students)
+            'link':     reverse('student-profile', args=[s.user.id])}, students)
 
 def prepare_ajax_employee_list(employees):
     return map(lambda e:
-            { 'id':  e.id,
+            { 'id':  e.user.id,
               'email': e.user.email,
               'name':     u'%s %s' % (e.user.first_name, e.user.last_name),
-              'link': reverse('employee-profile', args=[e.id]),
+              'link': reverse('employee-profile', args=[e.user.id]),
               'short_old': e.short_old,
               'short_new': e.short_new},
             employees)
