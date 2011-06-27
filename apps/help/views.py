ed4c66aecfc8af43f689eb613d7a096167f67e99
Dominika Rogozińska <dominika@dominika-Vostro-3300.(none)>
2011-06-27 12:45:21 +0200

    2nd commit: help with images

--- apps/help/views.py
+++ apps/help/views.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """
-    News views
+    Help views
 """
 
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render_to_response
+
+from django.contrib.admin.views.decorators import staff_member_required
+from apps.users.decorators import employee_required
 
 def main_page( request ):
     return render_to_response('help/base.html', {}, context_instance = RequestContext(request))
@@ -29,12 +32,14 @@ def mobile( request ):
 def export( request ):
     return render_to_response('help/export.html', {}, context_instance = RequestContext(request))
 
+def offer( request ):
+    return render_to_response('help/offer.html', {}, context_instance = RequestContext(request))
+
+@staff_member_required
 def admin( request ):
     return render_to_response('help/admin.html', {}, context_instance = RequestContext(request))
 
+@employee_required
 def employee( request ):
     return render_to_response('help/employee.html', {}, context_instance = RequestContext(request))
-
-def offer( request ):
-    return render_to_response('help/offer.html', {}, context_instance = RequestContext(request))
 
