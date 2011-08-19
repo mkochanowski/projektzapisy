e42fad76883999ff102091d44608577f71b1a01a
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-08-19 19:07:05 +0200

    FS#437 - Sortowanie grup po terminie

--- apps/enrollment/courses/views.py
+++ apps/enrollment/courses/views.py
@@ -64,7 +64,7 @@ def course(request, slug):
         course = Course.visible.get(slug=slug)
         records = Record.enrolled.filter(group__course=course)
         queues = Queue.queued.filter(group__course=course)
-        groups = list(Group.objects.filter(course=course))
+        groups = list(Group.objects.filter(course=course).order_by('term__dayOfWeek','term__start_time','term__end_time'))
         requirements = map(lambda x: x.name, course.requirements.all())
         if request.user.is_anonymous():
                 student = None
