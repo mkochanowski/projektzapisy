0f6a9f7cf1a511e4b7e33dbc3eb476a091fb0104
iks <iks@iks-Vostro-3700.(none)>
2011-09-17 10:59:12 +0200

    ocena

---
+++ apps/grade/ticket_create/managment/commands/ocen.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from apps.grade.ticket_create.models import StudentGraded
+from django.core.exceptions import ObjectDoesNotExist
+from apps.users.models import Student
+
+class Command(BaseCommand):
+    args = '<plik semester>'
+    help = 'ocenia'
+
+    def handle(self, *args, **options):
+        f = open(args[0], 'w')
+        for line in f:
+            u = StudentGraded()
+            try:
+                u.student = Student.objects.get(matricula=line)
+            except ObjectDoesNotExist:
+                pass
+            u.semester_id = args[1]
+            u.save()
