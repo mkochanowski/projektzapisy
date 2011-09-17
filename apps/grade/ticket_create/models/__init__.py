749eddd45a18efdabd82280749de68de2644373d
iks <iks@iks-Vostro-3700.(none)>
2011-09-17 10:34:22 +0200

    student_graded

--- apps/grade/ticket_create/models/__init__.py
+++ apps/grade/ticket_create/models/__init__.py
@@ -2,3 +2,4 @@ from used_ticket_stamp import UsedTicketStamp
 
 from private_key       import PrivateKey
 from public_key        import PublicKey
+from student_graded import StudentGraded
---
+++ apps/grade/ticket_create/models/student_graded.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+from django.db                import models
+from apps.users.models      import Student
+from apps.enrollment.courses.models import Semester
+
+class StudentGraded( models.Model ):
+    student = models.ForeignKey( Student,
+                                 verbose_name = "student" )
+    semester    = models.ForeignKey( Semester,
+                                 verbose_name = "semestr" )
+    class Meta:
+        verbose_name        = 'udzial w ocenie'
+        verbose_name_plural = 'udzial w ocenie'
+        app_label           = 'ticket_create'
+
+    def __unicode__(self):
+        return unicode( self.student ) + " " + unicode( self.semester )
+
