b3e60f9f828a3151727bb8ec18c0fa7c75de4167
Maciek <maciek@tardis.(none)>
2012-01-28 12:02:23 +0100

    admin tickets

--- apps/grade/ticket_create/admin.py
+++ apps/grade/ticket_create/admin.py
@@ -3,9 +3,14 @@ from django.contrib                    import admin
 from apps.grade.ticket_create.models import PublicKey, \
                                               PrivateKey, \
                                               UsedTicketStamp, StudentGraded
- 
+
+
+class StudentGradedAdmin( admin.ModelAdmin ):
+    list_display = ('student','semester')
+    search_fields = ('student__matricula','student__user__first_name','student__user__last_name','semester')
+
 admin.site.register( PublicKey )
 admin.site.register( PrivateKey )
 admin.site.register( UsedTicketStamp )                                              
-admin.site.register( StudentGraded )
+admin.site.register( StudentGraded, StudentGradedAdmin )
 
