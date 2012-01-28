cf366df8b988bc2ef72325f9eb4846005edc8a47
Maciek <maciek@tardis.(none)>
2012-01-28 12:07:07 +0100

    admin tickets

--- apps/grade/ticket_create/admin.py
+++ apps/grade/ticket_create/admin.py
@@ -6,7 +6,7 @@ from apps.grade.ticket_create.models import PublicKey, \
 
 
 class StudentGradedAdmin( admin.ModelAdmin ):
-    list_display = ('student','semester')
+    list_display = ('student', 'student__matricula', 'semester')
     search_fields = ('student__matricula','student__user__first_name','student__user__last_name','semester')
 
 admin.site.register( PublicKey )
