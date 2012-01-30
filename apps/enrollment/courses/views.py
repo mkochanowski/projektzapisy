3975c2fa3326390c7810bf14aeda7dbc9a5f894a
Maciek <maciek@tardis.(none)>
2012-01-30 18:21:45 +0100

    historia - skonczone

--- apps/enrollment/courses/views.py
+++ apps/enrollment/courses/views.py
@@ -29,20 +29,8 @@ def prepare_courses_list_to_render(request,default_semester=None,user=None, stud
     
    
     semesters = Semester.objects.filter(visible=True)
-    if hasattr(user, "student") and user.student.id == 351:
-        courses = Course.visible.all().order_by('name')\
-            .extra(select={'in_history': 'SELECT COUNT(*) FROM "records_record"' \
-                                         ' INNER JOIN "courses_group" ON ("records_record"."group_id" = "courses_group"."id")' \
-                                         ' INNER JOIN "courses_course" cc ON ("courses_group"."course_id" = cc."id")' \
-                                         ' WHERE (cc."entity_id" = "courses_course"."entity_id"  AND "records_record"."student_id" = '+ str(user.student.id)+ '' \
-                                                 ' AND "records_record"."status" = \'1\' AND "cc"."semester_id" <> "courses_course"."semester_id")'})
-        for c in courses:
-            c.was_enrolled = c.in_history > 0
-    else:
-        courses = Course.visible.all().order_by('name').values('id', 'name', 'type', 'slug', 'english', 'exam', 'suggested_for_first_year', 'semester')
-        for c in courses:
-            c['was_enrolled'] = False
 
+    courses = Course.visible.all().order_by('name').values('id', 'name', 'type', 'slug', 'english', 'exam', 'suggested_for_first_year', 'semester')
 
     semester_courses_list = {}
     semester_courses_list_setdefault = semester_courses_list.setdefault
@@ -104,15 +92,7 @@ def prepare_courses_list_to_render_and_return_course(request,default_semester=No
                 }
 
     semesters = Semester.objects.filter(visible=True)
-    if hasattr(user, "student") and user.student.id == 351:
-        courses = Course.visible.all().order_by('name')\
-        .extra(select={'in_history': 'SELECT COUNT(*) FROM "records_record"' \
-                                     ' INNER JOIN "courses_group" ON ("records_record"."group_id" = "courses_group"."id")' \
-                                     ' INNER JOIN "courses_course" cc ON ("courses_group"."course_id" = cc."id")' \
-                                     ' WHERE (cc."entity_id" = "courses_course"."entity_id"  AND "records_record"."student_id" = '+ str(user.student.id)+ '' \
-                                             ' AND "records_record"."status" = \'1\' AND "cc"."semester_id" <> "courses_course"."semester_id")'})
-    else:
-        courses = Course.visible.all().order_by('name')
+    courses = Course.visible.all().order_by('name')
 
 
     semester_courses_list = {}
