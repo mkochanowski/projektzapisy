22fa85014a5e366fcf6bf887d060f585a113932b
Maciek <maciek@tardis.(none)>
2012-01-30 18:05:07 +0100

    historia

--- apps/enrollment/courses/views.py
+++ apps/enrollment/courses/views.py
@@ -31,14 +31,18 @@ def prepare_courses_list_to_render(request,default_semester=None,user=None, stud
     semesters = Semester.objects.filter(visible=True)
     if hasattr(user, "student") and user.student.id == 351:
         courses = Course.visible.all().order_by('name')\
-            .extra(select={'was_enrolled': 'SELECT COUNT(*) > 0 FROM "records_record"' \
+            .extra(select={'in_history': 'SELECT COUNT(*) FROM "records_record"' \
                                          ' INNER JOIN "courses_group" ON ("records_record"."group_id" = "courses_group"."id")' \
                                          ' INNER JOIN "courses_course" cc ON ("courses_group"."course_id" = cc."id")' \
                                          ' WHERE (cc."entity_id" = "courses_course"."entity_id"  AND "records_record"."student_id" = '+ str(user.student.id)+ '' \
                                                  ' AND "records_record"."status" = \'1\' AND "cc"."semester_id" <> "courses_course"."semester_id")'})\
-            .values('id', 'name', 'type', 'slug', 'english', 'exam', 'suggested_for_first_year', 'semester', 'was_enrolled')
+            .values('id', 'name', 'type', 'slug', 'english', 'exam', 'suggested_for_first_year', 'semester', 'in_history')
+        for c in courses:
+            c.was_enrolled = c.in_history > 0
     else:
         courses = Course.visible.all().order_by('name').values('id', 'name', 'type', 'slug', 'english', 'exam', 'suggested_for_first_year', 'semester')
+        for c in courses:
+            c.was_enrolled = False
 
 
     semester_courses_list = {}
