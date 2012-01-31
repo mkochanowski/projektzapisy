01de8d7ac8f4259485db38d7c2dd77b8cc6adce4
Maciek <maciek@tardis.(none)>
2012-01-31 17:37:01 +0100

    filtry

--- apps/enrollment/courses/admin/admin.py
+++ apps/enrollment/courses/admin/admin.py
@@ -25,11 +25,11 @@ class CourseAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
 
-        if not request.GET.has_key('course__semester__id__exact'):
+        if not request.GET.has_key('semester__id__exact'):
 
             q = request.GET.copy()
             semester = Semester.get_current_semester()
-            q['course__semester__id__exact'] = semester.id
+            q['semester__id__exact'] = semester.id
             request.GET = q
             request.META['QUERY_STRING'] = request.GET.urlencode()
         return super(CourseAdmin,self).changelist_view(request, extra_context=extra_context)
