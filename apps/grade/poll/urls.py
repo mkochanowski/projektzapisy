77112b8a7b61e08cc9097dda87400b25b3e6ea6f
Maciek <maciek@tardis.(none)>
2012-02-29 10:12:34 +0100

    fix share

--- apps/grade/poll/urls.py
+++ apps/grade/poll/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     
     url(r'^poll_results/(?P<mode>[S,T])(?P<poll_id>[1-9][0-9]*)?$', 'apps.grade.poll.views.poll_results', name='grade-poll-poll-results'),
     url(r'^poll_results/(?P<mode>[S,T])(?P<poll_id>[1-9][0-9]*)/details/(?P<st_id>[1-9][0-9]*)?$', 'apps.grade.poll.views.poll_results_detailed', name='grade-poll-poll-results-detailed'),
-    url(r'^share_results/(?P<mode>[S,T])(?P<poll_id>[1-9][0-9]*)?$', 'apps.grade.poll.views.share_results_toggle', name='grade-poll-share-results'),
+    #url(r'^share_results/(?P<mode>[S,T])(?P<poll_id>[1-9][0-9]*)?$', 'apps.grade.poll.views.share_results_toggle', name='grade-poll-share-results'),
 #    url(r'^save_csv/(?P<mode>[S,T])(?P<poll_id>[1-9][0-9]*)?$', 'apps.grade.poll.views.save_csv', name='grade-poll-save-csv'),
 #
     url(r'^managment/polls_list/$', 'apps.grade.poll.views.polls_list', name='grade-poll-list'),
