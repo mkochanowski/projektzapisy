6fa8163c7fc5bd5bc53132bf5c636d1b318c9d2c
Maciek <maciek@tardis.(none)>
2012-05-25 14:30:38 +0200

    widget

--- apps/offer/proposal/forms.py
+++ apps/offer/proposal/forms.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.forms.models import ModelForm, inlineformset_factory, modelformset_factory
-from django.shortcuts import get_object_or_404
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.forms.models import ModelForm
 from apps.enrollment.courses.models.course import CourseEntity
-from apps.offer.proposal.models.book import Book
-from apps.offer.proposal.models.proposal import Proposal
-from apps.offer.proposal.models.proposal_description import ProposalDescription
 
 
 class ProposalForm(ModelForm):
-
     class Meta:
         exclude=('slug', 'owner', 'hidden', 'deleted', 'student')
         model = CourseEntity
+        widgets = {
+            'requirements': FilteredSelectMultiple("wymagania", is_stacked=False)
+        }
 
