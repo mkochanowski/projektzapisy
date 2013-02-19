a30d857bdb444a0b14cf6b5922d092f735a2f489
Maciek <maciek@tardis.(none)>
2013-02-19 13:15:07 +0100

    commit

--- zapisy/apps/enrollment/courses/admin/admin.py
+++ zapisy/apps/enrollment/courses/admin/admin.py
@@ -306,7 +306,7 @@ class GroupAdmin(admin.ModelAdmin):
             ev.type = '3'
             ev.visible = True
             ev.status = '1'
-            ev.author_id = obj.teacher
+            ev.author_id = obj.teacher.user
             ev.save()
 
             for room in t.classrooms.all():
