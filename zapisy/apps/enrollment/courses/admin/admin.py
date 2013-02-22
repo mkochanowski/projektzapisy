daae215e450232301254cf586b0f0217ec86e110
Maciek <maciek@tardis.(none)>
2013-02-22 09:52:22 +0100

    fix

--- zapisy/apps/enrollment/courses/admin/admin.py
+++ zapisy/apps/enrollment/courses/admin/admin.py
@@ -277,55 +277,57 @@ class GroupAdmin(admin.ModelAdmin):
                 Group.do_rearanged(obj)
 
         obj.save()
-        Event.objects.filter(group=obj, type='3').delete()
-        semester = obj.course.semester
 
-        freedays = Freeday.objects.filter(Q(day__gte=semester.lectures_beginning),
-                                          Q(day__lte=semester.lectures_ending))\
-                          .values_list('day', flat=True)
-        changed = ChangedDay.objects.filter(Q(day__gte=semester.lectures_beginning), Q(day__lte=semester.lectures_ending)).values_list('day', 'weekday')
-        terms = T.objects.filter(group=obj).select_related('group', 'group__course', 'group__course__courseentity')
-        days = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+        if obj:
+            Event.objects.filter(group=obj, type='3').delete()
+            semester = obj.course.semester
 
-        day = semester.lectures_beginning
+            freedays = Freeday.objects.filter(Q(day__gte=semester.lectures_beginning),
+                                              Q(day__lte=semester.lectures_ending))\
+                              .values_list('day', flat=True)
+            changed = ChangedDay.objects.filter(Q(day__gte=semester.lectures_beginning), Q(day__lte=semester.lectures_ending)).values_list('day', 'weekday')
+            terms = T.objects.filter(group=obj).select_related('group', 'group__course', 'group__course__courseentity')
+            days = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 
-        while day <= semester.lectures_ending:
+            day = semester.lectures_beginning
 
-            if day in freedays:
+            while day <= semester.lectures_ending:
+
+                if day in freedays:
+                    day = day + timedelta(days=1)
+                    continue
+
+                weekday = day.weekday()
+
+                for d in changed:
+                    if d[0] == day:
+                        weekday = int(d[1]) - 1
+                        break
+
+                days[weekday].append(day)
+
                 day = day + timedelta(days=1)
-                continue
 
-            weekday = day.weekday()
+            for t in terms:
+                ev = Event()
+                ev.group = t.group
+                ev.course = t.group.course
+                ev.title = ev.course.entity.get_short_name()
+                ev.type = '3'
+                ev.visible = True
+                ev.status = '1'
+                ev.author_id = obj.teacher.user.id
+                ev.save()
 
-            for d in changed:
-                if d[0] == day:
-                    weekday = int(d[1]) - 1
-                    break
-
-            days[weekday].append(day)
-
-            day = day + timedelta(days=1)
-
-        for t in terms:
-            ev = Event()
-            ev.group = t.group
-            ev.course = t.group.course
-            ev.title = ev.course.entity.get_short_name()
-            ev.type = '3'
-            ev.visible = True
-            ev.status = '1'
-            ev.author_id = obj.teacher.user.id
-            ev.save()
-
-            for room in t.classrooms.all():
-                for day in days[int(t.dayOfWeek) - 1]:
-                    newTerm = Term()
-                    newTerm.event = ev
-                    newTerm.day = day
-                    newTerm.start = timedelta(hours=t.start_time.hour, minutes=t.start_time.minute)
-                    newTerm.end = timedelta(hours=t.end_time.hour, minutes=t.end_time.minute)
-                    newTerm.room = room
-                    newTerm.save()
+                for room in t.classrooms.all():
+                    for day in days[int(t.dayOfWeek) - 1]:
+                        newTerm = Term()
+                        newTerm.event = ev
+                        newTerm.day = day
+                        newTerm.start = timedelta(hours=t.start_time.hour, minutes=t.start_time.minute)
+                        newTerm.end = timedelta(hours=t.end_time.hour, minutes=t.end_time.minute)
+                        newTerm.room = room
+                        newTerm.save()
 
 
 
