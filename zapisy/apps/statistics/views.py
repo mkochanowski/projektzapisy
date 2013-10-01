e71359eaae62796aacd26f41965758a17c7ffdc2
Maciek Jabłoński <maciej.jablonski@smartupz.com>
2013-10-01 16:02:19 +0200

    swaps

--- zapisy/apps/statistics/views.py
+++ zapisy/apps/statistics/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import permission_required
 from django.template.response import TemplateResponse
+from apps.enrollment.records.models import Queue, Record
 from apps.users.models import Student
-from apps.enrollment.courses.models import Semester, Group, CourseEntity
+from apps.enrollment.courses.models import Semester, Group, CourseEntity, Course
 
 
 @permission_required('courses.view_stats')
@@ -39,3 +40,45 @@ def groups(request):
 def votes(request):
     proposals = CourseEntity.statistics.in_year()
     return TemplateResponse(request, 'statistics/votes.html', locals())
+
+
+@permission_required('courses.view_stats')
+def swap(request):
+    semester = Semester.objects.get_next()
+    courses = Course.objects.filter(semester=semester)
+
+    types = [ ('2', 'ćwiczenia'), ('3', 'pracownia'),
+        ('5', 'ćwiczenio-pracownia'),
+        ('6', 'seminarium'), ('10', 'projekt')]
+
+    for course in Course.objects.filter(semester=semester):
+        course.groups_items = []
+
+        for type in types:
+            groups = Group.objects.filter(course=course, type=type[0])
+            students = {}
+            lists = {}
+            used = []
+
+            for g in groups:
+                lists[g.id] = []
+                for r in Record.objects.filter(group=g, status=1).select_related('student', 'student__user'):
+                    lists[g.id].append(r)
+                    students[r.student_id] = g
+
+            for group in groups:
+                group.swaps = []
+                queue = Queue.objects.filter(group=group, deleted=False).select_related('student', 'student__user')
+                for s in queue:
+                    for sp in lists[group.id]:
+                        if students[sp.student_id] == group and sp.student_id not in used:
+                            used.append(sp.student_id)
+                            group.swaps.append({
+                                'student_in_queue': sp.student,
+                                'student_in_group': s.student,
+                                'to': students[sp.student_id]
+                            })
+
+                course.groups_items.append(group)
+
+    return TemplateResponse(request, 'statistics/swap.html', locals())
