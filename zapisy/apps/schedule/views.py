8255b1430895a9db74b6fd9f0d8f3a5739a64570
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2016-04-17 16:06:48 +0200

    Fixed reservation form not showing for accepted and rejected events. Added automatic moderation messsage when changing event status through events from.

--- zapisy/apps/schedule/views.py
+++ zapisy/apps/schedule/views.py
@@ -118,14 +118,22 @@ def history(request):
 @require_POST
 def decision(request, event_id):
     from apps.schedule.models import Event
+    from .models.message import EventModerationMessage
 
     event = Event.get_event_for_moderation_only_or_404(event_id, request.user)
+    event_status = event.status
     form = DecisionForm(request.POST, instance=event)
     if form.is_valid():
-        form.save()
-        messages.success(request, u'Status wydarzenia został zmieniony')
-    else:
-        messages.error(request, u'Coś poszło źle')
+        if event_status == form.cleaned_data['status']:
+            messages.error(request, u'Status wydarzenia nie został zmieniony')
+        else:
+            event_obj = form.save()
+            msg = EventModerationMessage()
+            msg.author = request.user
+            msg.message = u'Status wydarzenia został zmieniony na ' + unicode(event_obj.get_status_display())
+            msg.event = event_obj
+            msg.save()
+            messages.success(request, u'Status wydarzenia został zmieniony')
 
     return redirect(reverse('events:show', args=[str(event.id)]))
 
