a03e82e9db452a13d687a055ee104d64cb950d65
Rafał Łasocha <me+dev@swistak35.com>
2016-11-06 01:23:30 +0100

    Turned off old, not working tests
    Maybe it's worth it to fix them, maybe it's easier to just delete them.
    Postponing this decision to future.
    
    Initialization of enrollment_tests file, so we can start writing new
    tests.

---
+++ zapisy/apps/enrollment/records/tests/enrollment_tests.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+from apps.enrollment.courses.models import Course, Group, StudentOptions, Semester
+
+class DummyTest(TestCase):
+    def testDummy(self):
+        self.assertTrue(True)
