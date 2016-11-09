445c4921bbf030e143ca70e4f02431f0fbbf2d29
Kamil Kamiński <kkaminski9311@gmail.com>
2016-11-09 19:24:52 +0100

    added test with voting in future, added test to circle.yml and updated changelog

--- zapisy/apps/offer/vote/tests.py
+++ zapisy/apps/offer/vote/tests.py
@@ -19,10 +19,17 @@ def create_active_system_state():
     )
 
 
-def create_inactive_system_state():
+def create_inactive_system_state_in_past():
     SystemState.objects.create(
         vote_beg=timezone.now() - timezone.timedelta(30),
         vote_end=timezone.now() - timezone.timedelta(10),
+    )
+
+
+def create_inactive_system_state_in_future():
+    SystemState.objects.create(
+        vote_beg=timezone.now() + timezone.timedelta(30),
+        vote_end=timezone.now() + timezone.timedelta(50),
     )
 
 
@@ -40,25 +47,40 @@ def generic_voting_active_view_test_case(urlname, testcase):
     testcase.assertContains(response, VOTE_LINK, 1, html=True)
 
 
-def generic_voting_inactive_view_test_case(urlname, testcase):
-    create_inactive_system_state()
+def _generic_voting_inactive_view_test_case(urlname, testcase):
     create_and_login_student(testcase)
     response = testcase.client.get(reverse(urlname))
     testcase.assertEquals(response.context['is_voting_active'], False)
     testcase.assertNotContains(response, VOTE_LINK, html=True)
 
 
+def generic_voting_inactive_view_past_test_case(urlname, testcase):
+    create_inactive_system_state_in_past()
+    _generic_voting_inactive_view_test_case(urlname, testcase)
+
+
+def generic_voting_inactive_view_future_test_case(urlname, testcase):
+    create_inactive_system_state_in_future()
+    _generic_voting_inactive_view_test_case(urlname, testcase)
+
+
 class VoteViewTestCase(TestCase):
     def test_vote_link_when_system_is_active(self):
         generic_voting_active_view_test_case('vote-view', self)
 
-    def test_vote_link_when_system_is_inactive(self):
-        generic_voting_inactive_view_test_case('vote-view', self)
+    def test_vote_link_when_system_is_inactive_in_past(self):
+        generic_voting_inactive_view_past_test_case('vote-view', self)
+
+    def test_vote_link_when_system_is_inactive_in_future(self):
+        generic_voting_inactive_view_future_test_case('vote-view', self)
 
 
 class VoteSummaryViewTestCase(TestCase):
     def test_vote_link_when_system_is_active(self):
         generic_voting_active_view_test_case('vote-summary', self)
 
-    def test_vote_link_when_system_is_inactive(self):
-        generic_voting_inactive_view_test_case('vote-summary', self)
+    def test_vote_link_when_system_is_inactive_in_past(self):
+        generic_voting_inactive_view_past_test_case('vote-summary', self)
+
+    def test_vote_link_when_system_is_inactive_in_future(self):
+        generic_voting_inactive_view_future_test_case('vote-summary', self)
