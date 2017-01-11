4c01eb38d6bc6596d544910ab138061f91b826d7
Rafał Łasocha <me@swistak35.com>
2017-01-11 02:25:35 +0100

    Another deprecation removed

--- zapisy/apps/email_change/urls.py
+++ zapisy/apps/email_change/urls.py
@@ -25,21 +25,17 @@
 #
 
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
     url(r'^email/change/$', 'apps.email_change.views.email_change_view', name='email_change'),
-    url(r'^email/verification/sent/$',
-        direct_to_template, {'template': 'email_change/email_verification_sent.html'},
-        name='email_verification_sent'),
+    url(r'^email/verification/sent/$', TemplateView.as_view(template_name="email_change/email_verification_sent.html")),
     # Note taken from django-registration
     # Verification keys get matched by \w+ instead of the more specific
     # [a-fA-F0-9]{40} because a bad verification key should still get to the view;
     # that way it can return a sensible "invalid key" message instead of a
     # confusing 404.
     url(r'^email/verify/(?P<verification_key>\w+)/$', 'apps.email_change.views.email_verify_view', name='email_verify'),
-    url(r'^email/change/complete/$',
-        direct_to_template, {'template': 'email_change/email_change_complete.html'},
-        name='email_change_complete'),
+    url(r'^email/change/complete/$', TemplateView.as_view(template_name="email_change/email_change_complete.html")),
 )
