b82702eab8b405d86484bb58fcfee527316517ff
Bartosz Kostka <kostka@weaselcrow.com>
2017-01-23 01:37:56 +0100

    ECTS limit tests... removed unnecessary (?) change

--- zapisy/apps/users/models.py
+++ zapisy/apps/users/models.py
@@ -684,7 +684,8 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class OpeningTimesView(models.Model):
-    student  = models.ForeignKey(Student, related_name='opening_times')
+    student  = models.ForeignKey(Student, primary_key=True,
+                                 related_name='opening_times')
     course   = models.ForeignKey('courses.Course')
     semester = models.ForeignKey('courses.Semester')
     opening_time = models.DateTimeField()
