f4da009176be9798d0e4ac7e1bfa16173d2d2b7a
Mateusz Lewandowski <mlewandowski@cs.uni.wroc.pl>
2017-02-24 15:06:25 +0100

    added dumb classroom creation

--- zapisy/apps/schedule/tests/testing_forms.py
+++ zapisy/apps/schedule/tests/testing_forms.py
@@ -3,10 +3,16 @@
 from django.test import TestCase
 
 from ..forms import EventForm
+from apps.enrollment.courses.tests.objectmothers import ClassroomObjectMother
 from apps.users.tests.factories import UserProfileFactory, UserFactory, EmployeeFactory
+from apps.enrollment.courses.models import Classroom
 
 
 class EventFormTestCase(TestCase):
+    def setUp(self):
+        room110 = ClassroomObjectMother.room110()
+        room110.save()
+
     def test_event_form_accepts_blank_description_when_user_is_employee(self):
         e = EmployeeFactory()
         UserProfileFactory(user=e.user, is_employee=True)
