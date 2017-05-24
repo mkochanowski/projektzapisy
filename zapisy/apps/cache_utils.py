1debcd107952e356510541ab9e87781ce0d3bda8
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-05-24 21:29:40 +0200

    pep8

--- zapisy/apps/cache_utils.py
+++ zapisy/apps/cache_utils.py
@@ -1,23 +1,26 @@
 """
 Implements a convenient way of caching the result
 of expensive functions: a simple decorator.
-Credits to: http://james.lin.net.nz/2011/09/08/python-decorator-caching-your-functions/
+Credits to:
+http://james.lin.net.nz/2011/09/08/python-decorator-caching-your-functions/
 """
 
 from django.core.cache import cache
- 
+
+
 # get the cache key for storage
 def cache_get_key(*args, **kwargs):
     import hashlib
     serialise = []
     for arg in args:
         serialise.append(str(arg))
-    for key,arg in kwargs.items():
+    for key, arg in kwargs.items():
         serialise.append(str(key))
         serialise.append(str(arg))
     key = hashlib.md5("".join(serialise)).hexdigest()
     return key
- 
+
+
 # decorator for caching functions
 def cache_result(time=0):
     def decorator(fn):
@@ -29,4 +32,4 @@ def cache_result(time=0):
                 cache.set(key, result, time)
             return result
         return wrapper
-    return decorator 
+    return decorator
