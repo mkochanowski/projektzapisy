1777de30326e1b5e4dd60d7bf8ac651b73a4d2a8
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-06-06 18:42:56 +0200

    dodane migracje, usuniety niepotrzebny plik

---
+++ zapisy/apps/enrollment/courses/migrations/0003_auto_20170606_1842.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0002_auto_20170601_1122'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='coursedescription',
+            name='created',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+        migrations.AlterField(
+            model_name='courseentity',
+            name='effects',
+            field=models.ManyToManyField(to='courses.Effects', verbose_name='Grupa efekt\xf3w kszta\u0142cenia', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='term',
+            name='classrooms',
+            field=models.ManyToManyField(related_name='new_classrooms', verbose_name=b'sale', to='courses.Classroom', blank=True),
+        ),
+    ]
