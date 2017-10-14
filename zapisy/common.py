74674a5d66146f8cfdd9d0cabe37fcd5ebf4a067
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-10-14 09:33:09 +0100

    Move some changes to other PRs

--- zapisy/common.py
+++ zapisy/common.py
@@ -8,10 +8,10 @@ FRIDAY = '5'
 SATURDAY = '6'
 SUNDAY = '7'
 
-DAYS_OF_WEEK = [(MONDAY, u'poniedziałek'),
+DAYS_OF_WEEK = [(MONDAY, u'poniedzialek'),
                 (TUESDAY, u'wtorek'),
-                (WEDNESDAY, u'środa'),
+                (WEDNESDAY, u'sroda'),
                 (THURSDAY, u'czwartek'),
-                (FRIDAY, u'piątek'),
+                (FRIDAY, u'piatek'),
                 (SATURDAY, u'sobota'),
                 (SUNDAY, u'niedziela')]
