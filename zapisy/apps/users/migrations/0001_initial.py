f7937e7bab2ff0db3cd7b20b94ab26b606b1bd4e
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-11-04 12:11:07 +0100

    Skipping django migrations is actually not required

--- zapisy/apps/users/migrations/0001_initial.py
+++ zapisy/apps/users/migrations/0001_initial.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        #('auth', '0001_initial'),
+        ('auth', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
