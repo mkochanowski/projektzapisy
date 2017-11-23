14dfc04300fa29abee385e1bfd5a398cc018ef89
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-11-23 11:49:33 +0100

    Start 1.10 upgrade

--- zapisy/apps/users/api_urls.py
+++ zapisy/apps/users/api_urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import *
+from django.conf.urls import url
 from apps.users.api import StudentList
 
-urlpatterns = patterns('',
-                       url('^student/$', StudentList.as_view())
-)
+urlpatterns = [
+    url('^student/$', StudentList.as_view())
+]
