51c9c2961da62e254068cc6dbcc6ca82467317a8
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-12-14 09:36:07 +0100

    Move typings to /node, add NODE_PATH to npm commands

--- env/setup_npm.sh
+++ env/setup_npm.sh
@@ -2,10 +2,13 @@
 # and will fail with EACCES
 echo "Installing npm packages"
 
+# Fake typings for tsc
+cp -R node/typings /node/
+
+# Needed for module resolving
+export NODE_PATH=/node/node_modules
+
 # package.json needs to be in the --prefixed dir
 cp /vagrant/zapisy/package.json /node/package.json
 
 npm i --prefix /node
-
-# Needed for module resolving
-export NODE_PATH=/node/node_modules
