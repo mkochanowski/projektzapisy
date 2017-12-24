11c5605b2924fe8cdbb3a2a9b371df121fc80027
Kuba Kazimierczak <kuba@kazimierczak.name>
2017-12-24 10:48:18 +0100

    Add rest framework and simple example

--- zapisy/apps/enrollment/courses/urls.py
+++ zapisy/apps/enrollment/courses/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from rest_api import test as test_rest_api
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'test', test_rest_api.UserViewSet)
+
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     url('^(?P<slug>[\w\-_]+)$', views.course, name='course-page'),
     url('^$', views.courses, name='course-list'),
     url('^$', views.courses, name='enrollment-main'),
