306438a4df4371e09ca676abce0807cd894ae000
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-01-11 09:12:30 +0100

    Remove forgotten url pattern

--- zapisy/apps/enrollment/courses/urls.py
+++ zapisy/apps/enrollment/courses/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
     url('^(?P<slug>[\w\-_]+)$', views.course, name='course-page'),
     url('^$', views.courses, name='course-list'),
     url('^$', views.courses, name='enrollment-main'),
