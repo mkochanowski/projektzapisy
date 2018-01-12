caaab58e96cf0fe8bee033dc579db2335a203643
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-01-12 22:18:28 +0100

    Make bootstrap script use npm.sh

--- env/setup_npm.sh
+++ env/setup_npm.sh
@@ -2,7 +2,5 @@
 # and will fail with EACCES
 echo "Installing npm packages"
 
-# package.json needs to be in the --prefixed dir
-cp /vagrant/zapisy/package.json /webpack_modules/package.json
-
-npm i --prefix /webpack_modules
+cd /vagrant/zapisy
+./npm.sh i
