a446de3e04ec4ddb5ba55b51c8a5b1abca586d82
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-01-22 18:49:24 +0100

    MIDDLEWARE_CLASSES -> MIDDLEWARE

--- zapisy/pipeline/middleware.py
+++ zapisy/pipeline/middleware.py
@@ -1,8 +1,8 @@
 from django.utils.encoding import DjangoUnicodeDecodeError
 from django.utils.html import strip_spaces_between_tags as minify_html
+from django.utils.deprecation import MiddlewareMixin
 
-
-class MinifyHTMLMiddleware(object):
+class MinifyHTMLMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if 'text/html' in response['Content-Type']:
             try:
