2d58146d00a518d8a465b65e25d33e507d74fefa
threaz <dampuk@gmail.com>
2018-01-31 21:03:19 +0100

    Amend path to absolute

--- env/bash_setup.sh
+++ env/bash_setup.sh
@@ -1,5 +1,5 @@
 echo "alias prepare-env='source /home/vagrant/env2.6/bin/activate'" >> .bash_aliases
 echo "alias prepare-env27='source /home/vagrant/env2.7/bin/activate'" >> .bash_aliases
 echo "alias get-settings='cp /vagrant/env/settings_local.py .'" >> .bash_aliases
-echo "alias run='python run.py'" >> .bash_aliases
+echo "alias run='python /vagrant/zapisy/run.py'" >> .bash_aliases
 echo "source /home/vagrant/env2.7/bin/activate" >> .bashrc
