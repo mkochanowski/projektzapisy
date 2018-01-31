8eb7379bf7d7212a4369752ba41a04bb0a33a503
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-01-31 19:30:36 +0100

    Fix error by replacing obsolete command class

--- zapisy/mailer/management/commands/retry_deferred.py
+++ zapisy/mailer/management/commands/retry_deferred.py
@@ -1,13 +1,13 @@
 import logging
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from mailer.models import Message
 
 logger = logging.getLogger('mailer.retry_deferred')
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = 'Attempt to resend any deferred mail.'
     
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         logger.info("-" * 72)        
         count = Message.objects.retry_deferred() # @@@ new_priority not yet supported
         logger.info("%s message(s) retried" % count)
