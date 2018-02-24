57955bc8179d2b078fb4fec65941f83c493d6442
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-02-24 21:59:34 +0100

    Add option to disable module dir creation

--- zapisy/npm.sh
+++ zapisy/npm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Ensure the directory exists and has proper perms
-./webpack_resources/create_modules_dir.sh
+if [ -z "$NO_CREATE_MODULES_DIR" ]; then
+    # Ensure the directory exists and has proper perms
+    ./webpack_resources/create_modules_dir.sh
+fi
 ./webpack_resources/run_npm_with_copy.sh $@
