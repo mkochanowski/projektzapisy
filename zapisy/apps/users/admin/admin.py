992e80c56e910a94dfb3d3467a34aa98a6524c29
Mikołaj Słupiński <mikolajslupinski@gmail.com>
2018-03-25 22:48:57 +0200

    fixup! Removed redundant information about ISIM students

--- zapisy/apps/users/admin/admin.py
+++ zapisy/apps/users/admin/admin.py
@@ -52,12 +52,12 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('matricula','get_full_name','ects','get_type_of_studies')
     fieldsets = [
         (None,               {'fields': ['user','matricula','status']}),
-        ('Studia', {'fields': ['numeryczna_l', 'dyskretna_l', 'program','semestr','ects', 'isim']}),
+        ('Studia', {'fields': ['numeryczna_l', 'dyskretna_l', 'program','semestr','ects']}),
         ('Zapisy', {'fields': ['records_opening_bonus_minutes','block']}),
         ('Inne', {'fields': ['receive_mass_mail_enrollment','receive_mass_mail_offer','receive_mass_mail_grade','last_news_view'], 'classes': ['collapse']}),
     ]
     search_fields = ('user__first_name', 'user__last_name', 'matricula')
-    list_filter = ('program','status','semestr', 'isim')
+    list_filter = ('program','status','semestr')
     ordering = ['user__last_name','user__first_name']
     list_display_links = ('get_full_name',)
     list_max_show_all = 9999
