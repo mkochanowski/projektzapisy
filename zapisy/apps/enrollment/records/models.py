f1e741e19a8f5111cf07bb2062d4ba3a886fc293
ApamNapat <bartlomiejbetka@gmail.com>
2018-04-22 22:03:40 +0200

    Ordered imports, removed commented code and unused functions

--- zapisy/apps/enrollment/records/models.py
+++ zapisy/apps/enrollment/records/models.py
@@ -1,23 +1,18 @@
+import logging
+from datetime import datetime, timedelta, date
+
 from django.core.exceptions import ValidationError
-from apps.enrollment.records.exceptions import NonStudentException, AlreadyPinnedException, \
-    AlreadyNotPinnedException, AlreadyNotAssignedException
-
-from apps.enrollment.courses.models.course import Course
-from apps.enrollment.courses.models.group import Group
-from apps.enrollment.courses.models.semester import Semester
-
 from django.contrib.auth.models import User
 from django.db import models
 
+from apps.enrollment.records.exceptions import NonStudentException, AlreadyPinnedException, \
+    AlreadyNotPinnedException, AlreadyNotAssignedException, NonGroupException
+from apps.enrollment.courses.models.course import Course
+from apps.enrollment.courses.models.group import Group
+from apps.enrollment.courses.models.semester import Semester
+from apps.enrollment.courses.exceptions import NonCourseException
 from apps.users.models import Student
 
-from apps.enrollment.records.exceptions import NonGroupException
-from apps.enrollment.courses.exceptions import NonCourseException
-
-from datetime import datetime, timedelta, date
-
-from django.db.models import signals
-import logging
 logger = logging.getLogger('project.default')
 backup_logger = logging.getLogger('project.backup')
 
@@ -558,22 +553,3 @@ class Queue(models.Model):
         return "%s (%s - %s)" % (self.group.course,
                                  self.group.get_type_display(),
                                  self.group.get_teacher_full_name())
-
-
-def log_add_record(sender, instance, created, **kwargs):
-    if instance.status == Record.STATUS_ENROLLED:
-        backup_logger.info(
-            '[01] user <%s> is added to group <%s>' %
-            (instance.student.user.id, instance.group.id))
-
-
-def log_delete_record(sender, instance, **kwargs):
-    if instance.status == Record.STATUS_ENROLLED and instance.group:
-        backup_logger.info('[03] user <%s> is removed from group <%s>' %
-                           (instance.student.user.id, instance.group.id))
-
-
-#signals.post_save.connect(log_add_record, sender=Record)
-#signals.pre_delete.connect(log_delete_record, sender=Record)
-#signals.post_delete.connect(Record.on_student_remove_from_group, sender=Record)
-#signals.post_save.connect(add_people_from_queue, sender=Group)
