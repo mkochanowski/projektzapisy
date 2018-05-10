843dc93e9849cd9f8e1dd38c35448424d184fa9d
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-05-10 20:00:17 +0200

    CR suggestions

--- db_backups/backup.py
+++ db_backups/backup.py
@@ -8,24 +8,26 @@ from datetime import datetime, timedelta
 import environ
 import dropbox
 
-
 from slack_notifications import (
-    initialize_slack, send_error_notification, send_success_notification
+    get_connected_slack_client, send_error_notification, send_success_notification
 )
 
 DROPBOX_DEV_DUMPS_DIRNAME = '/dev_dumps'
 DROPBOX_PROD_DUMPS_DIRNAME = '/prod_dumps'
 DUMPS_THRESHOLD = timedelta(weeks=-4)
 
+
 def get_script_dir():
     return os.path.dirname(os.path.realpath(__file__))
 
+
 def get_dump_filename(suffix: str):
     time_now_str = time.strftime('%Y_%m_%d__%H_%M_%S')
-    path = '/tmp/ii_zapisy_db_dump_{}'.format(time_now_str)
-    with_suffix = '{}_{}'.format(path, suffix) if suffix else path
+    path = f'/tmp/ii_zapisy_db_dump_{time_now_str}'
+    with_suffix = f'{path}_{suffix}' if suffix else path
     # Must end with 7z or 7za will append it itself
-    return "{}.7z".format(with_suffix)
+    return f'{with_suffix}.7z'
+
 
 def get_secrets_env():
     env = environ.Env()
@@ -45,12 +47,13 @@ def build_env_with_db_secrets(secrets_env):
     })
     return new_environ
 
+
 # The actual dumping is performed in a separate bash script
 # since interfacing with pg_dump/7za is easier there
 def perform_database_dump(out_dev_dump: str, out_prod_dump: str, secrets_env):
     new_environ = build_env_with_db_secrets(secrets_env)
     result = subprocess.run(
-        './perform_dump.sh {} {} > /dev/null'.format(out_dev_dump, out_prod_dump),
+        f'./perform_dump.sh {out_dev_dump} {out_prod_dump} > /dev/null',
         shell=True,
         stderr=subprocess.PIPE,
         env=new_environ,
@@ -58,16 +61,15 @@ def perform_database_dump(out_dev_dump: str, out_prod_dump: str, secrets_env):
     )
 
     if result.returncode != 0:
+        error_str = result.stderr.decode('utf-8')
         raise RuntimeError(
-            'perform_dump.sh failed with error code {}: {}'.format(
-                result.returncode, result.stderr.decode('utf-8'),
-            )
+            f'perform_dump.sh failed with error code {result.returncode}: {error_str}'
         )
-
 
 
 def get_dropbox_instance(secrets_env):
     return dropbox.Dropbox(secrets_env.str('DROPBOX_OAUTH2_TOKEN'))
+
 
 def dropbox_file_exists(dbx, path: str):
     try:
@@ -75,6 +77,7 @@ def dropbox_file_exists(dbx, path: str):
         return True
     except:
         return False
+
 
 def ensure_has_required_dirs(dbx):
     try:
@@ -86,6 +89,7 @@ def ensure_has_required_dirs(dbx):
     except dropbox.exceptions.ApiError:
         pass
 
+
 def remove_files_older_than_in_folder(dbx, threshold_dt, folder_path: str):
     list_folder_result = dbx.files_list_folder(folder_path)
     for file_entry in list_folder_result.entries:
@@ -93,22 +97,27 @@ def remove_files_older_than_in_folder(dbx, threshold_dt, folder_path: str):
         if file_date < threshold_dt:
             dbx.files_delete(file_entry.path_lower)
 
+
 def remove_dumps_older_than_threshold(dbx):
     oldest_allowed_modtime = datetime.now() + DUMPS_THRESHOLD
     remove_files_older_than_in_folder(dbx, oldest_allowed_modtime, DROPBOX_DEV_DUMPS_DIRNAME)
     remove_files_older_than_in_folder(dbx, oldest_allowed_modtime, DROPBOX_PROD_DUMPS_DIRNAME)
 
-# Upload a local file to a Dropbox folder, preserving the
-# file's name
+
 def upload_from_file_to_folder(dbx, file_path: str, folder_path: str) -> str:
+    """
+    Upload a local file to a Dropbox folder, preserving the
+    file's name
+    """
     file_bytes = open(file_path, mode='rb').read()
     file_name = os.path.basename(file_path)
     dropbox_path = os.path.join(folder_path, file_name)
     dbx.files_upload(file_bytes, dropbox_path)
     return dropbox_path
 
-# Uploads both dumps; returns the generated share link for the dev dump
+
 def upload_generated_dumps(dbx, dev_dump_path: str, prod_dump_path: str) -> str:
+    """ Uploads both dumps; returns the generated share link for the dev dump """
     ensure_has_required_dirs(dbx)
     remove_dumps_older_than_threshold(dbx)
     dev_dbx_path = upload_from_file_to_folder(dbx, dev_dump_path, DROPBOX_DEV_DUMPS_DIRNAME)
@@ -116,9 +125,11 @@ def upload_generated_dumps(dbx, dev_dump_path: str, prod_dump_path: str) -> str:
     return dbx.sharing_create_shared_link(dev_dbx_path)
 
 
-# Main backup routine. Dumps the DBs & uploads them to Dropbox.
-# Returns the generated share link for the dev dump
 def perform_full_backup(secrets_env) -> str:
+    """
+    Main backup routine. Dumps the DBs & uploads them to Dropbox.
+    Returns the generated share link for the dev dump
+    """
     dev_dump_filename = get_dump_filename('dev')
     prod_dump_filename = get_dump_filename('prod')
     perform_database_dump(dev_dump_filename, prod_dump_filename, secrets_env)
@@ -128,16 +139,20 @@ def perform_full_backup(secrets_env) -> str:
     os.remove(prod_dump_filename)
     return dev_db_shared_link
 
+
 def main():
+    secrets_env = get_secrets_env()
+    slack = get_connected_slack_client(secrets_env)
     try:
         start_time = datetime.now()
-        secrets_env = get_secrets_env()
-        initialize_slack(secrets_env)
         shared_link = perform_full_backup(secrets_env)
         end_time = datetime.now()
         seconds_elapsed = (end_time - start_time).seconds
-        send_success_notification(shared_link.url, seconds_elapsed)
+        send_success_notification(slack, shared_link.url, seconds_elapsed)
     except Exception:
         err_desc = traceback.format_exc()
-        send_error_notification(err_desc)
-main()
+        send_error_notification(slack, err_desc)
+
+
+if __name__ == "__main__":
+    main()
--- db_backups/slack_notifications.py
+++ db_backups/slack_notifications.py
@@ -3,33 +3,24 @@ from slackclient import SlackClient
 import time
 
 def get_connected_slack_client(secrets_env):
-    try:
-        slack_client = SlackClient(secrets_env.str('SLACK_TOKEN'))
-        if slack_client.rtm_connect(with_team_state=False):
-            return slack_client
-    except Exception:
-        pass
-    return None
+    slack_client = SlackClient(secrets_env.str('SLACK_TOKEN'))
+    if slack_client.rtm_connect(with_team_state=False):
+        return slack_client
+    raise RuntimeError('SlackClient.rtm_connect failed')
 
-def initialize_slack(secrets_env):
-    global slack_client
-    slack_client = get_connected_slack_client(secrets_env)
-
-def safe_send_msg(msg: str):
-    if slack_client is None:
-        return
+def _send_slack_msg(slack_client, msg: str):
     slack_client.api_call(
         "chat.postMessage",
         channel="db_backups",
         text=msg
     )
 
-def send_success_notification(dev_db_link: str, seconds_elapsed: int):
+def send_success_notification(slack_client, dev_db_link: str, seconds_elapsed: int):
     msg = "Databases backed up successfully in {} seconds. *Dev DB download link:* {}".format(
         seconds_elapsed, dev_db_link,
     )
-    safe_send_msg(msg)
+    _send_slack_msgslack_client, (msg)
 
-def send_error_notification(error_msg: str):
+def send_error_notification(slack_client, error_msg: str):
     msg = "*Failed to back up databases:* ```{}```".format(error_msg)
-    safe_send_msg(msg)
+    _send_slack_msg(slack_client, msg)
