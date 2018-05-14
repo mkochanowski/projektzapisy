a2679155e3ef1d20bb697ace531adcc732f2041e
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-05-14 19:07:40 +0200

    Add anonymize.sql

---
+++ db_backups/anonymize.sql
@@ -0,0 +1,15 @@
+-- Nuke IBANs
+UPDATE users_studiazamawiane SET bank_account='';
+
+-- Anonymize/remove email addresses
+UPDATE auth_user SET email='email@example.org';
+DELETE FROM mailer_message;
+DELETE FROM mailer_messagelog;
+
+-- Make sure everyone has the same simple password, in this case, 'pass'
+-- See here: https://docs.djangoproject.com/en/2.0/topics/auth/passwords/
+UPDATE auth_user SET password='pbkdf2_sha256$36000$Z6GlerjZ9cWC$M6zn6XGPc81913R1yw6SMouredUfO/DPnQwZ3XxUCnA=';
+
+-- Anonymize grade/poll answers
+UPDATE poll_openquestionanswer SET content = 'jakas ocena';
+DELETE FROM poll_singlechoicequestionanswer;
