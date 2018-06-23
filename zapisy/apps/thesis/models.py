f4ae148283aeac591e781d4d68126ebb0cf46353
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-06-23 15:17:16 +0200

    Add thesis app, first version of models.py

---
+++ zapisy/apps/thesis/models.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+from django.db import models
+
+from apps.users.models import Employee, Student
+
+
+MAX_THESIS_TITLE_LEN = 300
+
+class ThesisKind(Enum):
+	masters = 0
+	engineers = 1
+	bachelors = 2
+	bachelors_engineers = 3,
+	isim = 4
+THESIS_KIND_CHOICES = (
+	(ThesisKind.masters, "mgr"),
+	(ThesisKind.engineers, "inż"),
+	(ThesisKind.bachelors, "lic"),
+	(ThesisKind.bachelors_engineers, "lic+inż"),
+	(ThesisKind.isim, "isim"),
+)
+
+class ThesisStatus(Enum):
+	being_evaluated = 1,
+	returned_for_corrections = 2,
+	accepted = 3,
+	in_progress = 4,
+	defended = 5,
+THESIS_STATUS_CHOICES = (
+	(ThesisStatus.being_evaluated, "poddana pod głosowanie"),	
+	(ThesisStatus.returned_for_corrections, "zwrócona do poprawek"),
+	(ThesisStatus.accepted, "zaakceptowana"),
+	(ThesisStatus.in_progress, "w realizacji"),
+	(ThesisStatus.defended, "obroniona"),	
+)
+
+class Thesis(models.Model):
+	title = models.CharField(max_length=MAX_THESIS_TITLE_LEN)
+	advisor = models.ForeignKey(Employee, on_delete=models.PROTECT, null=True)
+	auxiliary_advisor = models.ForeignKey(Employee, on_delete=models.PROTECT, null=True)
+	kind = models.SmallIntegerField(choices=THESIS_KIND_CHOICES)
+	reserved = models.BooleanField(default=False)
+	student = models.ForeignKey(Student, on_delete=models.PROTECT, null=True)
+	student_2 = models.ForeignKey(Student, on_delete=models.PROTECT, null=True)
+	added_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+
+class ThesisCommissionMember(models.Model):
+	member = models.ForeignKey(Employee)
+
+
+class ThesisVote(Enum):
+	none = 1
+	rejected = 2
+	accepted = 3
+	user_missing = 4  # not sure about this one
+THESIS_VOTE_CHOICES = (
+	(ThesisVote.none, "brak głosu"),
+	(ThesisVote.rejected, "odrzucona"),
+	(ThesisVote.accepted, "zaakceptowana"),
+	(ThesisVote.user_missing, "brak użytkownika"),
+)
+
+class ThesisVoteBinding(models.Model):
+	thesis = models.ForeignKey(Thesis)
+	voter = models.ForeignKey(Employee)  # should be a member of the thesis commission
+	vote = models.SmallIntegerField(choices=THESIS_VOTE_CHOICES)
