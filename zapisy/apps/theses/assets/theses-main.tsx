fc693d49040e0fcb264d0992049a2fa85ec8a7cb
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-06-23 23:24:39 +0200

    Minor fixes, get react hello world working

--- zapisy/apps/theses/assets/theses-main.tsx
+++ zapisy/apps/theses/assets/theses-main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 window.addEventListener("DOMContentLoaded", () => {
 	ReactDOM.render(
 		<h1>Hello, world!</h1>,
-		document.getElementById("root"),
+		document.getElementById("theses-react-root"),
 	);
 });
