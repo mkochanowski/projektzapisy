a7f1b36f1cd5a420e8b53348372ecfe8587602d6
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-06-24 17:21:40 +0200

    Upgrade to webpack 4, merge the config files into one

--- zapisy/asset-defs.ts
+++ zapisy/asset-defs.ts
@@ -1,16 +1,10 @@
 module.exports = {
 	rawfiles: [
-		{ from: "legacy/css/", to: "css/" },
-		{ from: "legacy/help/", to: "help/" },
-		{ from: "legacy/images/", to: "images/" },
-		{ from: "legacy/js/", to: "js/" },
-		{ from: "legacy/vendor/", to: "vendor/" },
-		{ from: "legacy/favicon.ico", to: "favicon.ico" },
-		{ from: "legacy/feed-icon.png", to: "feed-icon.png" }
+		{ from: "legacy", to: "" },
 	],
 	bundles: {
-        "main": [
-            "common/libs.ts",
+		"main": [
+			"common/libs.ts",
 		],
 	},
 };
