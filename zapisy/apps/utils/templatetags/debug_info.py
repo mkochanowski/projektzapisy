76f3dc36a6994b7c5b92e9185d6f22db216cc3ce
Jasiek Marcinkowski <jasiekmarc@gmail.com>
2018-08-30 12:23:56 +0200

    Updates Django to 2.0.8. Fixes the debug_info.

--- zapisy/apps/utils/templatetags/debug_info.py
+++ zapisy/apps/utils/templatetags/debug_info.py
@@ -17,17 +17,26 @@ def get_line_from_process(process):
 
 
 @lru_cache(maxsize=1)
-@register.simple_tag
-def debug_info():
+def get_debug_info():
     """Queries environment for git commit, python and Django versions.
 
-    This information is displayed at the bottom of the main page in the debug
-    mode. The operation is time-consuming, so the result is being cached.
+    The operation is time-consuming, so the result is being cached.
     """
-    log_output = get_line_from_process(
-        ["git", "log", "-n", "1", "--pretty=format:%h %s --- %an %ad"])
+    log_output = get_line_from_process([
+        "git", "log", "-n", "1", "--pretty=format:%h %s --- %an %ad"
+    ])
     branch_name = get_line_from_process(["git", "rev-parse", "--abbrev-ref", "HEAD"])
     python_info = f'<p>Python <strong>{sys.version}</strong></p>'
     django_info = f'<p>Django <strong>{django.get_version()}</strong></p>'
     git_info = f'<p>{branch_name} {log_output}</p>'
     return f'{python_info}{django_info}{git_info}'
+
+
+@register.simple_tag
+def debug_info():
+    """A template tag to return debug info:
+
+    This information is displayed at the bottom of the main page in the debug
+    mode.
+    """
+    return get_debug_info()
