c432b1eb151d4a1d6e2026ea9606480884628180
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-11-07 15:58:42 +0100

    Update readme, fix top menu, remove unneeded logo

--- zapisy/apps/theses/__init__.py
+++ zapisy/apps/theses/__init__.py
@@ -1,8 +1,13 @@
 """
 TODO:
--> async table
--> move fields up
--> nuke datepicker
--> smaller save button (50% current height)
--> optional student/advisor fields
+-> remove uwr logo
+-> table: title, advisor left centered, alternating colors
+-> discard changes popup
+-> add new thesis: on click outside return to clear mode, also clear mode by default, button says "Add"
+-> textareas limit length
+-> votes: accept threshold, after enough (controlled in settings)
+board members accept we go into accepted, if someone rejects we go to rejected
+-> plus/minus icons...
+-> UI permissions obv
+-> up/down arrows switch between theses
 """
