851247e1bea6291a6f949892034e2a2da42fd5df
Pawel Bajgrowicz <bajgrowiczp@gmail.com>
2018-11-13 19:00:10 +0100

    Added route for notification view

--- zapisy/apps/notifications/urls.py
+++ zapisy/apps/notifications/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^notifications/save$', views.save, name='save'),
-    url(r'^notifications$', views.index, name='index'),
+    # url(r'^notifications$', views.index, name='index'),
     url(r'^notifications/vote_start$', views.vote_start,
         name='vote_start'),
     url(r'^notifications/grade_start$', views.grade_start,
