bcd5af34ce47f7990f881bce43b1148179b596ba
Artur Juraszek <artur@juraszek.xyz>
2018-11-15 12:40:18 +0100

    [#542] Introduce a Redis-backed implementation of NotificationsRepository

--- zapisy/apps/notifications2/repositories.py
+++ zapisy/apps/notifications2/repositories.py
@@ -2,9 +2,12 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import List
 
+from django.conf import settings
 from django.contrib.auth.models import User
+import redis
 
 from apps.notifications2.datatypes import Notification
+from apps.notifications2.serialization import NotificationSerializer
 
 
 class NotificationsRepository(ABC):
@@ -53,6 +56,66 @@ class FakeNotificationsRepository(NotificationsRepository):
 
     def mark_all_before_as_read(self, user: User, until: datetime) -> None:
         pass
+
+
+class RedisNotificationsRepository(NotificationsRepository):
+
+    def __init__(self, serializer: NotificationSerializer):
+        self.serializer = serializer
+        self.redis_client = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            password=settings.REDIS_PASS)
+
+    def get_count_for_user(self, user: User) -> int:
+        # SCARD returns 0 if one of them does not exist
+        # so no need to check for key existence here
+        unsent_count = self.redis_client.scard(
+            self._generate_unsent_key_for_user(user))
+        sent_count = self.redis_client.scard(
+            self._generate_sent_key_for_user(user))
+
+        return unsent_count + sent_count
+
+    def get_all_for_user(self, user: User) -> List[Notification]:
+        serialized = self.redis_client.smembers(
+            self._generate_unsent_key_for_user(user))
+        serialized = serialized.union(
+            self.redis_client.smembers(self._generate_sent_key_for_user(user)))
+
+        return list(map(self.serializer.deserialize, serialized))
+
+    def get_unsent_for_user(self, user: User) -> List[Notification]:
+        return list(map(
+            self.serializer.deserialize,
+            self.redis_client.smembers(self._generate_unsent_key_for_user(user))))
+
+    def save(self, user: User, notification: Notification) -> None:
+        self.redis_client.sadd(
+            self._generate_unsent_key_for_user(user),
+            self.serializer.serialize(notification))
+
+    def mark_all_before_as_read(self, user: User, until: datetime) -> None:
+        self._remove_all_older_than(
+            self._generate_unsent_key_for_user(user), until)
+        self._remove_all_older_than(
+            self._generate_sent_key_for_user(user), until)
+
+    def _remove_all_older_than(self, key: str, point_in_time: datetime) -> None:
+        notifications_under_that_key = map(
+            self.serializer.deserialize,
+            self.redis_client.smembers(key))
+
+        for notification in notifications_under_that_key:
+            if notification.issued_on < point_in_time:
+                self.redis_client.srem(
+                    key, self.serializer.serialize(notification))
+
+    def _generate_unsent_key_for_user(self, user: User) -> str:
+        return f'notifications_unsent_{user.id}'
+
+    def _generate_sent_key_for_user(self, user: User) -> str:
+        return f'notifications_sent_{user.id}'
 
 
 def get_current_repository_implementation() -> NotificationsRepository:
