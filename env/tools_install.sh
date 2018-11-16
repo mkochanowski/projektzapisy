9573908c1484ac0c7a39f29fb46cba0b72a82ae5
jasiekmarc <jasiekmarc@users.noreply.github.com>
2018-11-16 02:46:20 +0100

    Records 1/6: Simplifies Vagrant provisioning. Upgrades Ubuntu. Installs Redis. (#498)
    Ubuntu is updated to version 18.04 LTS. When setting up Vagrant box for development, Python is installed with Ubuntu package manager rather then compiled, which speeds up the provisioning. Redis server is being installed.
    
    A Windows symlink issue with `node_modules` folder is fixed by placing the folder outside of the shared filesystem and only mounting it at `/vagrant/zapisy/node_modules`.

--- env/tools_install.sh
+++ env/tools_install.sh
@@ -7,7 +7,6 @@ apt-get -y install libxml2-dev libxslt1-dev
 apt-get -y install libc6-dev
 apt-get -y install libjpeg62-dev
 apt-get -y install libfreetype6-dev
-apt-get -y install liblcms1-dev
 apt-get -y install xvfb
 apt-get -y install firefox # This package is apparently installed to get the requirements for the firefox we get in firefox_and_geckodriver_setup.sh
 apt-get -y install memcached
