186efa8b945be0e46a8c050525f756c5db88a7ed
SzymonMiler <44174849+SzymonMiler@users.noreply.github.com>
2018-11-25 17:42:12 +0100

    Update context_processors.py
    Dodałem pustą linię

--- zapisy/apps/notifications2/context_processors.py
+++ zapisy/apps/notifications2/context_processors.py
@@ -1,5 +1,6 @@
 from apps.notifications2.repositories import get_current_repository_implementation
 
+
 def notification_ico(request):
     repo = get_current_repository_implementation()
     return {"notification_counter": repo.get_count_for_user(request.user)}
