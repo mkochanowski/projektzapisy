16cd40fd1263ddc6741ac6c0da4eeb4b93966f96
Pawel Bajgrowicz <bajgrowiczp@gmail.com>
2018-11-30 15:26:26 +0100

    Added repository to view

--- zapisy/apps/notifications2/views.py
+++ zapisy/apps/notifications2/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from apps.notifications2.repositories import get_notifications_repository
 
 def index(request):
-    return render(request, 'notifications2/index.html')
+
+    repo = get_notifications_repository()
+    notfications = repo.get_all_for_user(request.user)
+
+    return render(request, 'notifications2/index.html', {'notifications': notfications})
