3e6bd01b0876483dc00f4110dbba365cbadffca8
Artur Juraszek <artur@juraszek.xyz>
2018-11-30 17:04:38 +0100

    Display notifications' content in proof of concept view

--- zapisy/apps/notifications2/views.py
+++ zapisy/apps/notifications2/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 from apps.notifications2.repositories import get_notifications_repository
+from apps.notifications2.utils import render_description
 
 def index(request):
 
     repo = get_notifications_repository()
-    notfications = repo.get_all_for_user(request.user)
+    notfications = [
+        render_description(notification.description_id, notification.description_args)
+        for notification in repo.get_all_for_user(request.user)
+    ]
 
     return render(request, 'notifications2/index.html', {'notifications': notfications})
