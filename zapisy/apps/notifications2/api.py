c592d732fa3339c7cb2d57b42b6e79795c85b4a8
Pawel Bajgrowicz <bajgrowiczp@gmail.com>
2018-12-03 23:09:30 +0100

    Notifier for groups of students

--- zapisy/apps/notifications2/api.py
+++ zapisy/apps/notifications2/api.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Dict
+from typing import Dict, List
 
 from django.contrib.auth.models import User
 
@@ -12,3 +12,8 @@ def notify_user(user: User, description_id: str, description_dict: Dict):
 
     repo.save(
         user, Notification(datetime.now(), description_id, description_dict))
+
+def notify_selected_users(users: List[User], description_id: str, description_dict: Dict):
+    for user in users:
+        notify_user(user, description_id, description_dict)
+
