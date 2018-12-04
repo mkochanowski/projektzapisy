153b88bc87088dc0d8423ffb0c93b539e9802d29
Pawel Bajgrowicz <bajgrowiczp@gmail.com>
2018-12-04 20:24:14 +0100

    Formatting fix

--- zapisy/apps/notifications2/api.py
+++ zapisy/apps/notifications2/api.py
@@ -13,7 +13,7 @@ def notify_user(user: User, description_id: str, description_dict: Dict):
     repo.save(
         user, Notification(datetime.now(), description_id, description_dict))
 
+
 def notify_selected_users(users: List[User], description_id: str, description_dict: Dict):
     for user in users:
         notify_user(user, description_id, description_dict)
-
