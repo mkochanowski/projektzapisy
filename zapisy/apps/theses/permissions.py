1ebb42d486c14faa14db023fa7270cf36dfa3562
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-12-08 14:13:55 +0100

    Start validating permissions on the backend

---
+++ zapisy/apps/theses/permissions.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+
+from .models import Thesis, ThesisStatus
+from .user_type import ThesisUserType, get_user_type
+
+
+def can_add_thesis(user_type: ThesisUserType) -> bool:
+    return user_type != ThesisUserType.student
+
+
+def is_thesis_staff(user_type: ThesisUserType) -> bool:
+    return user_type in [ThesisUserType.Admin, ThesisUserType.ThesesBoardMember]
+
+
+def can_modify_thesis(user: User, thesis: Thesis):
+    user_type = get_user_type(user)
+    if (is_thesis_staff(user_type)):
+        return True
+    return (
+        thesis.advisor and
+        thesis.advisor.pk == user.pk
+    )
+
+
+def can_set_status(user_type: ThesisUserType, status: ThesisStatus):
+    return (
+        is_thesis_staff(user_type) or
+        status == ThesisStatus.being_evaluated
+    )
+
+
+def can_set_advisor(user: User, user_type: ThesisUserType, advisor: User):
+    user_type = get_user_type(user)
+    if (is_thesis_staff(user_type)):
+        return True
+    return user.pk == advisor.pk
--- zapisy/apps/theses/serializers.py
+++ zapisy/apps/theses/serializers.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from . import models
 from apps.users.models import Employee, Student
+from .models import Thesis, ThesisStatus
 from .errors import InvalidQueryError
 from .user_type import get_user_type
+from .permissions import can_set_status, can_set_advisor
 
 
 class PersonSerializerForThesis(serializers.Serializer):
@@ -26,6 +27,11 @@ class PersonSerializerForThesis(serializers.Serializer):
         return {"id": person_id}
 
 
+def copy_if_present(dst, src, key, converter=None):
+    if key in src:
+        dst[key] = converter(src[key]) if converter else src[key]
+
+
 class ThesisSerializer(serializers.ModelSerializer):
     advisor = PersonSerializerForThesis(allow_null=True, required=False)
     auxiliary_advisor = PersonSerializerForThesis(allow_null=True, required=False)
@@ -34,17 +40,51 @@ class ThesisSerializer(serializers.ModelSerializer):
     added_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S%z", required=False)
     modified_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S%z", required=False)
 
+    def validate(self, data):
+        # self.context will not be defined for local serialization
+        # no special validation needs to be performed then (drf already provides
+        # basic validation based on the constraints above)
+        if not self.context:
+            return data
+        request = self.context["request"]
+        if request.method == "POST":
+            return self.validate_add_thesis(request.user, data)
+        elif request.method == "PATCH":
+            return self.validate_modify_thesis(request.user, data)
+        else:
+            raise serializers.ValidationError(f'Unknown request type {request.method}')
+
+    def validate_add_thesis(user: User, data: dict):
+        user_type = get_user_type(user)
+        advisor = get_person(Employee, data["advisor"])
+        if not can_set_advisor(user, user_type, advisor):
+            raise serializers.ValidationError(f'This type of user cannot set advisor to {advisor}')
+        status = data["status"]
+        if not can_set_status(user_type, status):
+            raise serializers.ValidationError(f'This type of user cannot set status to {status}')
+        result = {
+            "title": data["title"],
+            "reserved": data["reserved"],
+            "kind": data["kind"],
+            "status": status,
+        }
+        copy_if_present(result, data, "description")
+        copy_if_present(result, data, "auxiliary_advisor", lambda a: get_person(Employee, a))
+        copy_if_present(result, data, "student", lambda s: get_person(Student, s))
+        copy_if_present(result, data, "student_2", lambda s: get_person(Student, s))
+        return result
+
     def create(self, validated_data):
-        new_instance = models.Thesis(
+        new_instance = Thesis(
             title=validated_data.get("title"),
             kind=validated_data.get("kind"),
             status=validated_data.get("status"),
             reserved=validated_data.get("reserved"),
+            advisor=validated_data.get("advisor"),
+            auxiliary_advisor=validated_data.get("auxiliary_advisor")
         )
         if "description" in validated_data:
             new_instance.description = validated_data.get("description")
-        ThesisSerializer._assign_advisors(new_instance, validated_data)
-        ThesisSerializer._assign_students(new_instance, validated_data)
         new_instance.save()
         return new_instance
 
@@ -76,7 +116,7 @@ class ThesisSerializer(serializers.ModelSerializer):
             instance.student_2 = _get_person_from_queryset(Student, validated_data.get("student_2"))
 
     class Meta:
-        model = models.Thesis
+        model = Thesis
         read_only_fields = ('id', )
         fields = (
             'id', 'title', 'advisor', 'auxiliary_advisor',
@@ -85,11 +125,7 @@ class ThesisSerializer(serializers.ModelSerializer):
         )
 
 
-def _get_person_from_queryset(queryset, person_data):
-    if person_data is None:
-        return None
-    if "id" not in person_data:
-        raise InvalidQueryError("Missing person ID")
+def get_person(queryset, person_data):
     try:
         return queryset.objects.get(pk=person_data.get("id"))
     except queryset.DoesNotExist:
