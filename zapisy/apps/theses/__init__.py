92635093c9c169981dc78a6583ca90fb78a77ad0
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-12-09 12:17:03 +0100

    Update todo

--- zapisy/apps/theses/__init__.py
+++ zapisy/apps/theses/__init__.py
@@ -1,8 +1,6 @@
 """
 TODO:
 -> add new thesis: on click outside return to clear mode, also clear mode by default, button says "Add"
--> textareas limit length
 -> votes: accept threshold, after enough (controlled in settings)
 board members accept we go into accepted, if someone rejects we go to rejected
--> UI permissions obv
 """
