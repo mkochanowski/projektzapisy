739d36ce49b5d07712229bdf0ec89e4807229954
Wojciech Meller <wojmel97@gmail.com>
2018-12-18 15:05:20 +0100

    Add KaTex and update /grade main page

--- zapisy/asset-defs.ts
+++ zapisy/asset-defs.ts
@@ -7,6 +7,8 @@ export default {
 		{ from: "legacy/vendor/", to: "vendor/" },
 		{ from: "legacy/favicon.ico", to: "favicon.ico" },
 		{ from: "legacy/feed-icon.png", to: "feed-icon.png" },
+		{ from: "legacy/katex/", to: "katex/" },
+		{ from: "legacy/grade/", to: "grade/" },
 	],
 	bundles: {
 		"main": [
