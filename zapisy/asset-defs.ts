41517d083095a77b1e29047182fb21fb7706d6b1
Wojciech Meller <wojmel97@gmail.com>
2018-12-18 15:05:20 +0100

    Add KaTex and update /grade main page

--- zapisy/asset-defs.ts
+++ zapisy/asset-defs.ts
@@ -1,28 +1,18 @@
 export default {
-    rawfiles: [
-        { from: "legacy/css/", to: "css/" },
-        { from: "legacy/help/", to: "help/" },
-        { from: "legacy/images/", to: "images/" },
-        { from: "legacy/js/", to: "js/" },
-        { from: "legacy/vendor/", to: "vendor/" },
-        { from: "legacy/favicon.ico", to: "favicon.ico" },
-        { from: "legacy/feed-icon.png", to: "feed-icon.png" },
-    ],
-    bundles: {
-        "main": [
-            "common/expose_libs.ts",
-            "common/_variables.scss",
-            "common/index.scss",
-            "common/cookieconsent/display-cookieconsent.ts",
-        ],
-        "fullcalendar": [
-            "common/fullcalendar.ts"
-        ],
-        "filtered-select-multiple-converter": [
-            "common/filtered-select-multiple-converter.js"
-        ],
-        "consent-dialog": [
-            "common/consent-dialog.ts"
-        ]
-    },
+	rawfiles: [
+		{ from: "legacy/css/", to: "css/" },
+		{ from: "legacy/help/", to: "help/" },
+		{ from: "legacy/images/", to: "images/" },
+		{ from: "legacy/js/", to: "js/" },
+		{ from: "legacy/vendor/", to: "vendor/" },
+		{ from: "legacy/favicon.ico", to: "favicon.ico" },
+		{ from: "legacy/feed-icon.png", to: "feed-icon.png" },
+		{ from: "legacy/katex/", to: "katex/" },
+		{ from: "legacy/grade/", to: "grade/" },
+	],
+	bundles: {
+		"main": [
+			"common/libs.ts",
+		],
+	},
 };
