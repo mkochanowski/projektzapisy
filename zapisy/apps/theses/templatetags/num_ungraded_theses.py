5ed512d8d1f2839be41a048df15c90dc92b09019
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-12-27 21:34:22 +0100

    Check for invalid users to fix selenium tests

--- zapisy/apps/theses/templatetags/num_ungraded_theses.py
+++ zapisy/apps/theses/templatetags/num_ungraded_theses.py
@@ -1,4 +1,6 @@
 from django import template
+
+from apps.users.models import BaseUser
 from ..users import wrap_user, is_theses_board_member
 from ..models import get_num_ungraded_for_emp
 
@@ -15,6 +17,11 @@ def num_ungraded_theses(user):
     if not user.is_authenticated:
         return 0
     wrapped_user = wrap_user(user)
+    # FIXME temporary fix for dumb selenium tests where this
+    # can happen as they don't create user instances properly
+    # To be removed when selenium tests are disabled
+    if not isinstance(wrapped_user, BaseUser):
+        return 0
     if not is_theses_board_member(wrapped_user):
         return 0
     return get_num_ungraded_for_emp(wrapped_user)
