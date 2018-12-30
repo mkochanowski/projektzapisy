3f2a9d3f90fbe9df03c90448e7b2b4799c7801d0
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-12-30 14:09:25 +0100

    Update babel/dependencies to support jsx, update ts compiler settings, update linter settings

--- zapisy/webpack_resources/custom_declarations/misc.ts
+++ zapisy/webpack_resources/custom_declarations/misc.ts
@@ -1,9 +1,15 @@
+// Allow importing images through url/file loader
 declare module "*.png" {
 	const content: string;
 	export default content;
 }
 
 declare module "*.gif" {
+	const content: string;
+	export default content;
+}
+
+declare module "*.ico" {
 	const content: string;
 	export default content;
 }
---
+++ zapisy/webpack_resources/custom_declarations/typeless_libs.ts
@@ -0,0 +1,7 @@
+// This file contains empty type stubs for modules without typings
+// This is needed because we have noImplicitAny enabled in tsconfig
+
+// We want to be able to import any .js file
+declare module "*.js";
+// ...or jsx
+declare module "*.jsx";
