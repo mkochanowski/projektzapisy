c617fe3bf186c4df3e39a4599aa99e247aee891f
Kuba Kazimierczak <kuba@kazimierczak.name>
2019-01-03 23:53:16 +0100

    Add scripts

---
+++ zapisy/scripts/add_test_theses.py
@@ -0,0 +1,70 @@
+import random
+
+from faker import Faker
+
+from apps.theses.models import Thesis, ThesisKind, ThesisStatus
+from apps.users.models import Employee, Student
+
+NUM_THESES = 1000
+
+
+fake = Faker()
+
+
+def random_bool():
+    return bool(random.getrandbits(1))
+
+
+def random_title():
+    return f'{fake.name()}_{random.randrange(123123)}'
+
+
+def random_advisor(emps):
+    return random.choice(emps)
+
+
+valid_kinds = [
+    kind.value for kind in ThesisKind
+]
+def random_kind():  # noqa: E302
+    return random.choice(valid_kinds)
+
+
+valid_statuses = [
+    status.value for status in ThesisStatus
+]
+def random_status():  # noqa: E302
+    return random.choice(valid_statuses)
+
+
+def random_reserved():
+    return random_bool()
+
+
+def random_description():
+    return fake.text()
+
+
+def random_student(studs):
+    return random.choice(studs)
+
+
+def run():
+    Thesis.objects.all().delete()
+    studs = Student.objects.all()
+    emps = Employee.objects.all()
+    theses = [
+        Thesis(
+            title=random_title(),
+            advisor=random_advisor(emps),
+            auxiliary_advisor=random_advisor(emps) if random_bool() else None,
+            kind=random_kind(),
+            status=random_status(),
+            reserved=random_reserved(),
+            description=random_description(),
+            student=random_student(studs),
+            student_2=random_student(studs) if random_bool() else None
+        ) for _ in range(NUM_THESES)
+    ]
+    Thesis.objects.bulk_create(theses)
+    print(f'Created {NUM_THESES} instances')
---
+++ zapisy/scripts/add_thesis_votes.py
@@ -0,0 +1,22 @@
+import random
+from apps.theses.models import Thesis, ThesisVoteBinding, ThesisVote
+from apps.theses.users import get_theses_board
+
+
+def run():
+    ThesisVoteBinding.objects.all().delete()
+
+    cnt = 0
+    board_members = list(get_theses_board())
+    board_members_cnt = len(board_members)
+    for thesis in Thesis.objects.all():
+        num_votes = random.randint(board_members_cnt // 2, board_members_cnt)
+        members_to_vote = random.sample(board_members, num_votes)
+        votes = [
+            (voter, ThesisVote(random.randint(2, 3)))
+            for voter in members_to_vote
+        ]
+        thesis.process_new_votes(votes)
+        cnt += len(votes)
+
+    print(f'Created {cnt} instances in total')
