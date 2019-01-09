b8d17a8cc5d474fbfaddf967e7c5dc42929aafed
Wojciech Meller <wojmel97@gmail.com>
2019-01-09 22:15:50 +0100

    Fix importing css in bundle katex

--- zapisy/apps/grade/asset-defs.ts
+++ zapisy/apps/grade/asset-defs.ts
@@ -1,13 +1,10 @@
 export default {
    bundles: {
       "katex": [
-         "../../../node_modules/katex/dist/katex.css",
-         "katex.less",
          "katex.ts",
       ],
    },
    rawfiles: [
-      // { from: "grade-client.py", to: "../grade-client.py" }, 
       // "grade-client.py",
    ],
 };
