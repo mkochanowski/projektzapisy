2d878ca4170782c3d1c8fa48230d375bab059102
Wojciech Meller <wojmel97@gmail.com>
2019-01-09 11:19:06 +0100

    Fix Katex as node module

---
+++ zapisy/apps/grade/asset-defs.ts
@@ -0,0 +1,13 @@
+export default {
+   bundles: {
+      "katex": [
+         "../../../node_modules/katex/dist/katex.css",
+         "katex.less",
+         "katex.ts",
+      ],
+   },
+   rawfiles: [
+      // { from: "grade-client.py", to: "../grade-client.py" }, 
+      // "grade-client.py",
+   ],
+};
