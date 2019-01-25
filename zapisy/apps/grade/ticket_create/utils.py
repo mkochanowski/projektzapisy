275e73b16599d7a8942af3b9d1bcd75ed58f2a57
Wojciech Balik <wb2602171@gmail.com>
2019-01-26 00:12:39 +0100

    fixing style

--- zapisy/apps/grade/ticket_create/utils.py
+++ zapisy/apps/grade/ticket_create/utils.py
@@ -276,6 +276,7 @@ def secure_signer_without_save(user, g, t):
     except ValueError:
         return "Niepoprawny format"
 
+
 def match_signing_requests_with_polls(signing_requests, user):
     """
     For each signing request, matches it with poll corresponding to provided id, checking
@@ -305,9 +306,6 @@ def validate_tickets(signing_requests):
         except ValueError:
             continue
     return res
-
-
-    
 
 
 def secure_mark(user, poll):
