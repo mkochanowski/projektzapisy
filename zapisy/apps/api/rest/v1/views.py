1f984f0bb8385c57c3c96aad3795e0b98acf8c2c
Mateusz Całka <saalin@outlook.com>
2019-01-28 21:22:12 +0100

    Adds API listing students' votes (#528)
    Api had been requested in #480 and #481. 
    (Closes #480; Closes #481)

--- zapisy/apps/api/rest/v1/views.py
+++ zapisy/apps/api/rest/v1/views.py
@@ -5,13 +5,15 @@ from rest_framework.authentication import (BasicAuthentication,
 from apps.enrollment.courses.models.classroom import Classroom
 from apps.enrollment.courses.models.semester import Semester
 from apps.offer.desiderata.models import Desiderata, DesiderataOther
+from apps.offer.vote.models import SystemState, SingleVote
 from apps.schedule.models.specialreservation import SpecialReservation
 from apps.users.models import Employee
 from apps.users.utils import StaffPermission
 
 from .serializers import (ClassroomSerializer, DesiderataOtherSerializer,
                           DesiderataSerializer, EmployeeSerializer,
-                          SemesterSerializer, SpecialReservationSerializer)
+                          SemesterSerializer, SpecialReservationSerializer,
+                          SingleVoteSerializer, SystemStateSerializer)
 
 
 class SemesterViewSet(viewsets.ModelViewSet):
@@ -53,4 +55,34 @@ class SpecialReservationViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
     queryset = SpecialReservation.objects.all()
     serializer_class = SpecialReservationSerializer
+    filter_fields = '__all__'
+
+
+class SingleVoteViewSet(viewsets.ModelViewSet):
+    """Returns votes by selected state (or all votes otherwise).
+
+    State is passed by GET parameter (e.g. url?state=n). Skips votes with no
+    value for clarity.
+    """
+    http_method_names = ['get']
+    permission_classes = (StaffPermission,)
+    serializer_class = SingleVoteSerializer
+    filter_fields = '__all__'
+
+    def get_queryset(self):
+        queryset = SingleVote.objects.select_related('entity').exclude(value=0, correction=0)
+        system_state_id = self.request.GET.get('state')
+        if system_state_id:
+            queryset = queryset.filter(state_id=system_state_id)
+
+        return queryset
+
+
+class SystemStateViewSet(viewsets.ModelViewSet):
+    """Get all vote system states"""
+
+    http_method_names = ['get']
+    permission_classes = (StaffPermission,)
+    queryset = SystemState.objects.all()
+    serializer_class = SystemStateSerializer
     filter_fields = '__all__'
