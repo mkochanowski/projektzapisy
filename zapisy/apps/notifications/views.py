d11ee7ed871ccc5b358c84aa90456ba8462c064b
SzymonMiler <44174849+SzymonMiler@users.noreply.github.com>
2019-02-06 13:02:39 +0100

    poprawki

--- zapisy/apps/notifications/views.py
+++ zapisy/apps/notifications/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
 from apps.users.models import BaseUser
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from apps.notifications.forms import PreferencesFormStudent, PreferencesFormTeacher
@@ -32,15 +34,14 @@ def index(request):
 @require_POST
 @login_required
 def preferences_save(request):
-    if request.method == "POST":
-        form = create_form(request)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user
-            post.save()
-            return redirect('/notifications/preferences')
-        else:
-            messages.error(request, "Wystąpił błąd, zmiany nie zostały zapisane. Proszę wypełnić formularz ponownie")
+    form = create_form(request)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.user = request.user
+        post.save()
+        return HttpResponseRedirect(reverse('notifications:preferences'))
+    else:
+        messages.error(request, "Wystąpił błąd, zmiany nie zostały zapisane. Proszę wypełnić formularz ponownie")
 
 
 def preferences(request):
@@ -49,6 +50,9 @@ def preferences(request):
 
 
 def create_form(request):
+    """
+        It is not a view itself, just factory for preferences and preferences_save
+    """
     if BaseUser.is_employee(request.user):
         instance, created = NotificationPreferencesTeacher.objects.get_or_create(user=request.user)
         if request.method == 'POST':
