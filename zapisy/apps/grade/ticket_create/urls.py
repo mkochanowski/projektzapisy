d8f0699808dfce56d8bdcdb8a68eb686161f7a18
Wojciech Balik <wb2602171@gmail.com>
2019-02-09 15:29:15 +0100

    removing old comment

--- zapisy/apps/grade/ticket_create/urls.py
+++ zapisy/apps/grade/ticket_create/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     url(r'^ajax_get_keys$', views.ajax_get_rsa_keys, name='grade-ticket-get-keys'),
     url(r'^ajax_sign_tickets$', views.ajax_sign_tickets, name='grade-ticket-ajax-sign-tickets'),
     url(r'^keys_generate$', views.keys_generate, name='grade-ticket-keys-generate'),
-
-    # feature do poprawienia w przyszłości
     url(r'^tickets_generate$', views.tickets_generate, name='grade-ticket-tickets-generate')
 ]
