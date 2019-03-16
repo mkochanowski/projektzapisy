838cd95c3988aacdabd0fbbc637d1e4ff08df590
Wojciech Balik <wb2602171@gmail.com>
2019-03-16 15:56:34 +0100

    moved signature verification function to public key model

--- zapisy/apps/grade/ticket_create/models/public_key.py
+++ zapisy/apps/grade/ticket_create/models/public_key.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
 
 class PublicKey(models.Model):
+    '''Public key of a poll, encoded in PEM format.'''
     poll = models.ForeignKey('poll.Poll', verbose_name='ankieta', on_delete=models.CASCADE)
     public_key = models.TextField(verbose_name='klucz publiczny')
 
@@ -12,3 +14,11 @@ class PublicKey(models.Model):
 
     def __str__(self):
         return "Klucz publiczny: " + str(self.poll)
+
+    def verify_signature(self, ticket: int, signature: int) -> bool:
+        pk = RSA.importKey(self.public_key)
+        signature_pow_e = pow(signature, pk.e, pk.n)
+        ticket_hash = SHA256.new(str(ticket).encode()).hexdigest()
+        ticket_hash_as_int = int(ticket_hash, 16)
+
+        return ticket_hash_as_int == signature_pow_e
