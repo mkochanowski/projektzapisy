5e6cc136d323eefb85cbf8ab8bb000f8195a4876
Wojciech Balik <wb2602171@gmail.com>
2019-03-24 15:34:37 +0100

    refactored ticket parsing

--- zapisy/apps/grade/ticket_create/serializers.py
+++ zapisy/apps/grade/ticket_create/serializers.py
@@ -8,3 +8,16 @@ class SigningRequestSerializer(serializers.Serializer):
 
 class SigningRequestsListSerializer(serializers.Serializer):
     signing_requests = serializers.ListField(child=SigningRequestSerializer())
+
+
+class TicketSerializer(serializers.Serializer):
+    name = serializers.CharField(required=False)
+    type = serializers.CharField(required=False)
+    teacher = serializers.CharField(required=False)
+    id = serializers.IntegerField(required=True, min_value=0)
+    ticket = serializers.IntegerField(required=True, min_value=0)
+    signature = serializers.IntegerField(required=True, min_value=0)
+
+
+class TicketsListSerializer(serializers.Serializer):
+    tickets = serializers.ListField(child=TicketSerializer())
