e91b4cf911e16db836fca43598f0a439550330a3
Wojciech Balik <wb2602171@gmail.com>
2019-03-31 16:57:56 +0200

    rewritten client side of the protocol to typescript

--- zapisy/apps/grade/asset-defs.ts
+++ zapisy/apps/grade/asset-defs.ts
@@ -4,7 +4,7 @@ export default {
          "katex.ts",
       ],
       "ticketsgenerate": [
-         "ticketsgenerate.js",
+         "ticketsgenerate.ts",
       ],
    },
    rawfiles: [
--- zapisy/apps/grade/ticket_create/urls.py
+++ zapisy/apps/grade/ticket_create/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^ajax_keys_progress$', views.ajax_keys_progress, name='grade-ticket-keys-progress'),
     url(r'^ajax_keys_generate$', views.ajax_keys_generate, name='grade-ticket-keys-generator'),
-    url(r'^ajax_get_keys$', views.ajax_get_rsa_keys, name='grade-ticket-get-keys'),
-    url(r'^ajax_sign_tickets$', views.ajax_sign_tickets, name='grade-ticket-ajax-sign-tickets'),
+    url(r'^get-poll-data$', views.get_poll_data, name='grade-ticket-get-poll-data'),
+    url(r'^sign-tickets$', views.sign_tickets, name='grade-ticket-sign-tickets'),
     url(r'^keys_generate$', views.keys_generate, name='grade-ticket-keys-generate'),
     url(r'^tickets_generate$', views.tickets_generate, name='grade-ticket-tickets-generate')
 ]
