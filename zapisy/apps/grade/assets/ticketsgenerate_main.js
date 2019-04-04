5834e0ccfb74a991067406f3366d744d1819b1f9
Wojciech Balik <wb2602171@gmail.com>
2019-04-04 11:55:26 +0200

    minor code style improvements

--- zapisy/apps/grade/assets/ticketsgenerate_main.js
+++ zapisy/apps/grade/assets/ticketsgenerate_main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import TicketsGenerator from './TicketsGenerator.vue'
 
 Vue.config.productionTip = false;
 
 window.onload = function() {
     new Vue({
         el: '#app',
-        render: h => h(App)
+        render: h => h(TicketsGenerator)
     });
 };
