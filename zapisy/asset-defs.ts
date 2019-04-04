5f96a4cbd7d093b47f3d1913526c4612f03c36cd
Wojciech Balik <wb2602171@gmail.com>
2019-04-04 15:10:43 +0200

    merging from master-dev

--- zapisy/asset-defs.ts
+++ zapisy/asset-defs.ts
@@ -1,16 +1,28 @@
 export default {
-	rawfiles: [
-		{ from: "legacy/css/", to: "css/" },
-		{ from: "legacy/help/", to: "help/" },
-		{ from: "legacy/images/", to: "images/" },
-		{ from: "legacy/js/", to: "js/" },
-		{ from: "legacy/vendor/", to: "vendor/" },
-		{ from: "legacy/favicon.ico", to: "favicon.ico" },
-		{ from: "legacy/feed-icon.png", to: "feed-icon.png" },
-	],
-	bundles: {
-		"main": [
-			"common/libs.ts",
-		],
-	},
+    rawfiles: [
+        { from: "legacy/css/", to: "css/" },
+        { from: "legacy/help/", to: "help/" },
+        { from: "legacy/images/", to: "images/" },
+        { from: "legacy/js/", to: "js/" },
+        { from: "legacy/vendor/", to: "vendor/" },
+        { from: "legacy/favicon.ico", to: "favicon.ico" },
+        { from: "legacy/feed-icon.png", to: "feed-icon.png" },
+    ],
+    bundles: {
+        "main": [
+            "common/expose_libs.ts",
+            "common/_variables.scss",
+            "common/index.scss",
+            "common/cookieconsent/display-cookieconsent.ts",
+        ],
+        "fullcalendar": [
+            "common/fullcalendar.ts"
+        ],
+        "filtered-select-multiple-converter": [
+            "common/filtered-select-multiple-converter.js"
+        ],
+        "consent-dialog": [
+            "common/consent-dialog.ts"
+        ]
+    },
 };
