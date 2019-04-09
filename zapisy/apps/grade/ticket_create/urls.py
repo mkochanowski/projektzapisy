7bb1ce8c9c2959b13c544eab25d8691662fd37d0
Wojciech Balik <wb2602171@gmail.com>
2019-04-09 10:25:59 +0200

    migrating from url to path

--- zapisy/apps/grade/ticket_create/urls.py
+++ zapisy/apps/grade/ticket_create/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^ajax_keys_progress$', views.ajax_keys_progress, name='grade-ticket-keys-progress'),
-    url(r'^ajax_keys_generate$', views.ajax_keys_generate, name='grade-ticket-keys-generator'),
-    url(r'^get-poll-data$', views.get_poll_data, name='grade-ticket-get-poll-data'),
-    url(r'^sign-tickets$', views.sign_tickets, name='grade-ticket-sign-tickets'),
-    url(r'^keys_generate$', views.keys_generate, name='grade-ticket-keys-generate'),
-    url(r'^tickets_generate$', views.tickets_generate, name='grade-ticket-tickets-generate')
+    path('ajax_keys_progress', views.ajax_keys_progress, name='grade-ticket-keys-progress'),
+    path('ajax_keys_generate', views.ajax_keys_generate, name='grade-ticket-keys-generator'),
+    path('get-poll-data', views.get_poll_data, name='grade-ticket-get-poll-data'),
+    path('sign-tickets', views.sign_tickets, name='grade-ticket-sign-tickets'),
+    path('keys_generate', views.keys_generate, name='grade-ticket-keys-generate'),
+    path('tickets_generate', views.tickets_generate, name='grade-ticket-tickets-generate')
 ]
