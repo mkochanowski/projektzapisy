e25be4a6c7ea4c89ce126771ab02a79017efd116
Marek Kochanowski <marek@kochanow.ski>
2019-04-09 21:22:55 +0200

    Update base Dockerfile

---
+++ docker_before_postgres.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+#install tzdata package
+apt-get install -y tzdata
+# set your timezone
+ln -fs /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
