eb7d43c63e0efa0f89f18bb784f09729789d6e1a
Wojciech Balik <wb2602171@gmail.com>
2019-04-09 13:21:08 +0200

    removed unused code

--- zapisy/apps/grade/ticket_create/urls.py
+++ zapisy/apps/grade/ticket_create/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('get-poll-data', views.get_poll_data, name='grade-ticket-get-poll-data'),
     path('sign-tickets', views.sign_tickets, name='grade-ticket-sign-tickets'),
-    path('keys-generate', views.keys_generate, name='grade-ticket-keys-generate'),
     path('tickets-generate', views.tickets_generate, name='grade-ticket-tickets-generate')
 ]
