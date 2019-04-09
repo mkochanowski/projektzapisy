e0a5fc5e9103ee30ee6d67e6c5a71797ad442645
Wojciech Balik <wb2602171@gmail.com>
2019-04-09 10:50:12 +0200

    removing unused code

--- zapisy/apps/grade/ticket_create/urls.py
+++ zapisy/apps/grade/ticket_create/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ajax_keys_progress', views.ajax_keys_progress, name='grade-ticket-keys-progress'),
-    path('ajax_keys_generate', views.ajax_keys_generate, name='grade-ticket-keys-generator'),
     path('get-poll-data', views.get_poll_data, name='grade-ticket-get-poll-data'),
     path('sign-tickets', views.sign_tickets, name='grade-ticket-sign-tickets'),
-    path('keys_generate', views.keys_generate, name='grade-ticket-keys-generate'),
-    path('tickets_generate', views.tickets_generate, name='grade-ticket-tickets-generate')
+    path('keys-generate', views.keys_generate, name='grade-ticket-keys-generate'),
+    path('tickets-generate', views.tickets_generate, name='grade-ticket-tickets-generate')
 ]
