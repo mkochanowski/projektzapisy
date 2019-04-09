c0c2dbac715e2a71350a490a9451e00474c72897
Wojciech Balik <wb2602171@gmail.com>
2019-04-09 13:13:10 +0200

    added create_key signal

---
+++ zapisy/apps/grade/ticket_create/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from apps.grade.poll.models import Poll
+from apps.grade.ticket_create.models import SigningKey
+
+
+@receiver(post_save, sender=Poll)
+def generate_key(sender: Poll, instance: Poll, **kwargs) -> None:
+    pem_rsa_key = SigningKey.generate_rsa_key()
+    key = SigningKey(poll=instance, private_key=pem_rsa_key)
+    key.save()
