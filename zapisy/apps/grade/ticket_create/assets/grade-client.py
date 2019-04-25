56305be3b9b2b23ef7dec0f3103e97c903a32bb5
Wojciech Balik <wb2602171@gmail.com>
2019-04-25 21:15:58 +0200

    rewritten grade client

--- zapisy/apps/grade/ticket_create/assets/grade-client.py
+++ zapisy/apps/grade/ticket_create/assets/grade-client.py
@@ -1,328 +1,182 @@
 #!/usr/bin/env python
-
-"""Przykładowy klient tworzący karty do oceny
-
-    Moduł zawiera przykładową implementację
-    generowania kluczy do oceny zajęć w klasie Tickets
-    oraz funkcje pomocnicze.
-    Uruchomienie pliku rozpoczyna proces tworzenia kart.
-"""
-from typing import Union, Iterable, Type, Mapping, Dict, NoReturn
-from io import TextIOBase
-from getpass import getpass
-from sys import stdout, stderr, argv
-from math import gcd
-from random import getrandbits
-from hashlib import sha256
-import requests
 import os
+import argparse
 import json
+from math import gcd
+from secrets import randbelow
+from typing import Dict
+from getpass import getpass
+from urllib.parse import urljoin
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Util.number import inverse
+import requests
 
-Poll = Dict[str, Union[int, Mapping[str, Union[str, int]]]]
-
-
-def rand(bits: int) -> int:
-    """Zwraca losową liczbę.
-
-    Zwraca bits-bitową losową liczbę całkowitą (bits>=1)
-    najbardziej znaczący bit jest ustawiony.
-    """
-
-    return getrandbits(bits) | 1 << (bits - 1)
-
-
-def modinv(a: int, n: int) -> int:
-    """Odwrotność modulo.
-
-    Zwraca t,
-    a*t = 1 mod n.
-    Alogrytm Euklidesa.
-    """
-
-    t, nt = 0, 1
-    r, nr = n, a
-    while nr != 0:
-        q = r // nr
-        t, nt = nt, t - q * nt
-        r, nr = nr, r - q * nr
-    if r > 1:
-        return None
-    if t < 0:
-        return t + n
-    return t
+URL = os.getenv('URL', 'https://zapisy.ii.uni.wroc.pl/')
 
 
-def convert_key(key: Mapping[str, str]) -> Dict[str, int]:
-    """Zmienia wartości klucza na liczby."""
-
-    return {
-        'n': int(key['n']),
-        'e': int(key['e'])
-    }
-
-
-def try_again() -> bool:
-    """Pyta użytkownika czy próbować ponownie."""
-
-    res = input("Próbować jeszcze raz? [T/n]:")
-    if not res or res[0] == 't' or res[0] == 'T':
-        return True
-    else:
-        return False
+def hash_(m: int) -> int:
+    '''We are using hash function to disable multiplicative properties
+    of RSA encryption/decryption. In other words, if E(m) is standard rsa 
+    encryption/decryption, then E(m*k) = E(m) * E(k), but if we set
+    E'(m) = E(H(m)), where H is hash function, then E'(m*k) != E'(m) * E'(k).
+    If we didn't use that, you would be able to generate many tickets, just
+    by knowing factors of m.
+    '''
+    h = SHA256.new()
+    h.update(str(m).encode())
+    return int(h.hexdigest(), 16)
 
 
-class Tickets:
-    """Główna logika generowania kluczy do oceny zajęć."""
+def blind(pub_key, m, r):
+    return (m * pow(r, pub_key.e, pub_key.n)) % pub_key.n
 
-    url: str = os.getenv('URL', 'https://zapisy.ii.uni.wroc.pl/')
-    # Szablon do wypisania kluczy.
-    template: str = ("[{title}] {name} \nid: {id} \n{ticket} \n{signed_ticket} \n" +
-                     ('-' * 34) + " \n")
 
-    def __init__(self, out_file: Type[TextIOBase], backup_file: Type[TextIOBase]) -> None:
-        """Tworzy obiekt tworzący karty do głosowania.
+def unblind(pub_key, m, r):
+    return (m * inverse(r, pub_key.n)) % pub_key.n
 
-        Aby uzyskać karty należy użyć metody run.
 
-        Args:
-            out_file: do tego będą zapisane klucze w finalnym formacie.
-            backup_file: do tego będą zapisane dane diagnostyczne
-                i surowe dane podpisanych kart.
-        """
+class Ticket:
+    '''Class which is responsible for generating and holding ticket data.'''
+    def __init__(self, pub_key):
+        # This while loop could be omitted, since condition GCD(r, pub_key.n) != 1 will probably
+        # never happen in our universe lifetime, but lets do that for the sake of correctness.
+        while True:
+            m = randbelow(pub_key.n)
+            r = randbelow(pub_key.n)
+            if gcd(r, pub_key.n) == 1:
+                break
+        self.m = m
+        self.r = r
 
-        # Połączenie z serwerem jest obsługiwane przez bibiliotekę requests.
-        self.client: Type[requests.sessions.Session] = requests.session()
-        self.poll_data: Iterable[Mapping[str, Union[Poll, Mapping[str, int]]]]
-        self.data: Dict[int, Poll] = dict()
-        self.signed_blind_tickets: Iterable[Mapping[str, Union[int, str]]]
-        self.out_file = out_file
-        self.backup_file = backup_file
 
-    def run(self) -> None:
-        """Uruchamia tworzenie kart.
+class PollData:
+    '''This class serves the purpose of holding data related to poll in little bit
+    of a higher level than just a raw dictionary'''
+    def __init__(self, poll: Dict, ticket=None):
+        self.pub_key = RSA.construct((int(poll['key']['n']), int(poll['key']['e'])))
+        self.name = poll['poll_info']['name']
+        self.type = poll['poll_info']['type']
+        self.id = poll['poll_info']['id']
+        self.ticket = ticket
 
-        Uruchamia metody w odpowiedniej kolejności
-        oraz informuje użytkownika o aktualnym stanie.
-        """
+    def get_signing_request(self) -> Dict:
+        assert self.ticket is not None
+        ticket_to_sign = blind(self.pub_key, hash_(self.ticket.m), self.ticket.r)
+        return {
+            'ticket': str(ticket_to_sign),
+            'id': self.id,
+        }
 
-        # Logowanie jest wymagane do pobrania kluczy publicznych
-        # oraz do podpisania kluczy do oceny.
-        while not self.login():
-            print("Nie Zalogowano")
-        print("Zalogowano")
 
-        while not self.get_public_keys():
-            print("Nie udało się pobrać kluczy")
-            if not try_again():
-                return
-        print("Udało się pobrać klucze")
+class TicketCreate:
+    '''Main logic.'''
+    def __init__(self, url):
+        self.url = url
+        self.client = requests.Session()
+        res = self.client.get(self.url)
+        res.raise_for_status()
 
-        print("Generowanie kart")
-        self.prepare_tickets()
-        print("Karty wygenerowane")
+    def _post(self, path: str, *args, **kwargs) -> requests.Response:
+        '''Wrapper for self.client.post.'''
+        if 'headers' not in kwargs:
+            kwargs['headers'] = {}
+        kwargs['headers']['X-CSRFToken'] = self.csrf_token
+        response = self.client.post(urljoin(self.url, path), *args, **kwargs)
+        assert response.ok
+        return response
 
-        while not self.get_signed_tickets():
-            print("Nie udało się uzyskać podpisanych kart")
-            print("Podpisać karty można tylko raz, możliwe że już to zrobił(aś/eś)")
-            if not try_again():
-                return
-        print("Udało się uzyskać podpisane karty")
-
-        print("Odślepianie podpisanych kart")
-        self.unblind_tickets()
-        print("Podpisane karty odślepione")
-
-        print("Zapis kart")
-        self.save()
-        print("Gotowe")
-
-    def login(self) -> bool:
-        """Logowanie"""
-
-        # Weź pierwszy CSRF token.
+    def login(self) -> None:
         self.client.get(self.url)
-
-        # Wyślij dane do logowania z tokenem CSRF.
+        username = input('Username: ')
+        password = getpass('Password: ')
         data = {
-            'username': self.username,
-            'password': self.password,
-            'csrfmiddlewaretoken': self.csrftoken
+            'username': username,
+            'password': password,
         }
 
-        response = self.client.post(self.url + 'users/login/', data=data)
-        # Proste sprawdzenie czy logowanie się powiodło.
-        return response.ok and response.text.find(
-            '/users/password-change/') != -1
+        response = self._post('/users/login/', data=data)
+        assert response.ok
+        login_success_check = self._post('/grade/ticket/tickets-generate', allow_redirects=False)
+        if login_success_check.status_code != 200:
+            raise RuntimeError("Login failed")
 
-    def get_public_keys(self) -> bool:
-        """Pobranie kluczy publicznych ankiet."""
 
-        # Należy wysłać token CSRF.
+    def get_polls(self) -> Dict[int, PollData]:
+        '''First step of the protocol, query the server for polls metadata(name, type, id)
+        and public keys.
+
+        Returns:
+            Dict with id as a key, and as a value, data received from the server,
+            wrapped in PollData class.
+        '''
+        res = self._post('/grade/ticket/get-poll-data')
+        polls = res.json()
+        return {
+            poll['poll_info']['id']: PollData(poll)
+            for poll in polls['poll_data']
+        }
+
+    def get_signed_tickets(self, polls: Dict[int, PollData]):
+        '''Second step of the protocol, after generating tickets, sends them
+        in blinded form to the server, so it can sign them.
+        '''
         data = {
-            'csrfmiddlewaretoken': self.csrftoken
+            'signing_requests': []
         }
 
-        response = self.client.post(
-            self.url + 'grade/ticket/get-poll-data', data=data)
+        for id_, poll_data in polls.items():
+            ticket = Ticket(poll_data.pub_key)
+            poll_data.ticket = ticket
+            signing_request = poll_data.get_signing_request()
+            data['signing_requests'].append(signing_request)
+        
+        return data
 
-        if response.ok:
-            # Zwrócone dane to lista obiektów
-            # [{'key': {'n': string, 'e': string }, 'pool_info': X }]
-            # X jest obiektem z conajmniej polem 'title' oraz 'id'
-            # może mieć również pola 'type', 'course_name', 'teacher_name'
-            # jeśli ich nie ma jest to ankieta ogólna.
-            self.poll_data = response.json()['poll_data']
-            return True
-        return False
-
-    @staticmethod
-    def generate_ticket(n: int) -> Dict[str, int]:
-        """Tworzy kartę i kopertę."""
-
-        bits = n.bit_length()
-
-        randomize = True
-        while randomize:
-            m = rand(bits)
-            k = rand(bits)
-            randomize = k > n or m > n or gcd(k, n) != 1
-        return {'k': k, 'm': m}
-
-    @staticmethod
-    def blind_ticket(ticket: Mapping[str, int], public_key: Mapping[str, int]) -> int:
-        """Zaślepia pojedynczy klucz/ wsadza kartę do koperty."""
-
-        m_sha256 = int(sha256(str(ticket['m']).encode('ascii')).hexdigest(), 16)
-        k_pow_e = pow(ticket['k'], public_key['e'], public_key['n'])
-        t = (m_sha256 * k_pow_e) % public_key['n']
-        return t
-
-    def prepare_tickets(self) -> None:
-        """Generowanie kluczy/kart i zaślepianie/wsadzanie do kopert do podpisania."""
-
-        for data in self.poll_data:
-            poll = {
-                'public_key': convert_key(data['key']),
-                'poll_info': data['poll_info']
-            }
-
-            ticket = Tickets.generate_ticket(poll['public_key']['n'])
-
-            poll['m'] = ticket['m']
-            poll['k'] = ticket['k']
-            poll['t'] = Tickets.blind_ticket(ticket, poll['public_key'])
-            self.data[data['poll_info']['id']] = poll
-
-        del self.poll_data
-
-    def get_signed_tickets(self) -> bool:
-        """Wyślij karty w kopertach do podpisania."""
-
-        signing_requests = [{
-            'id': str(id),
-            'ticket': str(poll['t']),
-        } for id, poll in self.data.items()]
-
-        data = {
-            'signing_requests': signing_requests
-        }
-
-        headers = {
-            'X-Csrftoken': self.csrftoken,
-        }
-        response = self.client.post(
-            self.url + 'grade/ticket/sign-tickets', json=data, headers=headers)
-
-        if response.ok:
-            # Zwrócone dane to lista obiektów
-            # [{
-            #    'status': 'OK'|'ERROR',
-            #    'id': int,
-            #    'message'|'signature': string,
-            # }]
-            # wartość 'message' to informacja o błędzie
-            # obecna gdy status to 'ERROR',
-            # wartość 'signature' to podpisana karta w kopercie
-            # obecna gdy status to 'OK'.
-            self.signed_blind_tickets = response.json()
-            # Karty można podpisać tylko raz
-            # dlatego nie przetworzoną odpowiedź należy zapisać,
-            # aby móc wznowić w razie problemów.
-            self.backup_file.write(response.text)
-            return True
-        return False
-
-    def unblind_tickets(self) -> None:
-        """Wyjęcie podpisanych kart z kopert."""
-        for data in self.signed_blind_tickets:
-            id = data['id']
-            if data['status'] == 'OK':
-                st = int(data['signature'])
-                public_key = self.data[id]['public_key']
-                rk = modinv(self.data[id]['k'], public_key['n'])
-                self.data[id]['s'] = (st * rk) % public_key['n']
-            else:
-                self.backup_file.write("Błąd id: {id} \t| {message}\n".format(**data))
-                del self.data[id]
-
-        del self.signed_blind_tickets
-
-    def save(self) -> None:
-        """Zapisanie kopert w ustalonym formacie."""
-        out_data = {
+    def get_tickets(self):
+        '''Main function.'''
+        self.login()
+        polls = self.get_polls()
+        signing_request = self.get_signed_tickets(polls)
+        res = self._post('/grade/ticket/sign-tickets', json=signing_request).json()
+        tickets_for_user = {
             'tickets': []
         }
-        for id, poll in self.data.items():
-            data = {
-                'type': poll['poll_info']['type'],
-                'name': poll['poll_info']['name'],
-                'id': id,
-                'ticket': str(poll['m']),
-                'signature': poll['s'],
-            }
-            out_data['tickets'].append(data)
-        self.out_file.write(json.dumps(out_data, indent=2))
+        for signed_ticket in res:
+            if signed_ticket['status'] == 'ERROR':
+                print('ERROR: {}'.format(signed_ticket['message']))
+                continue
+            poll_data = polls[signed_ticket['id']]
+            unblinded_signature = unblind(poll_data.pub_key, int(signed_ticket['signature']), poll_data.ticket.r)
+            tickets_for_user['tickets'].append({
+                'name': poll_data.name,
+                'type': poll_data.type,
+                'id': poll_data.id,
+                'ticket': str(poll_data.ticket.m),
+                'signature': str(unblinded_signature),
+            })
+        return tickets_for_user
 
     @property
-    def csrftoken(self) -> Union[str, NoReturn]:
+    def csrf_token(self):
         """get CSRF Token"""
         if 'csrftoken' in self.client.cookies:
             return self.client.cookies['csrftoken']
-        raise Exception("Brak tokenu CSRF")
-
-    @property
-    def username(self) -> str:
-        """get username"""
-        return input('Login: ')
-
-    @property
-    def password(self) -> str:
-        """get password"""
-        return getpass('Password: ')
+        raise AttributeError("CSRF token not found")
 
 
-USAGE = """
-Użycie: {} [opcje] [plik_z_kartami [plik_backup]]
-plik_z_kartami domyślnie to standardowe wyjście
-plik_backup domyślnie to standardowe wyjście błędów
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('f', type=str, help='Path to the output file.')
+    args = parser.parse_args()
 
-Opcje:
-  -h, --help \tWyświetla ten tekst i kończy działanie.
-"""
-if __name__ == "__main__":
-    if '-h' in argv or '--help' in argv:
-        print(USAGE.format(argv[0]))
-    else:
-        FILE = stdout
-        BACKUP = stderr
-        if len(argv) > 1:
-            FILE = open(argv[1], 'w')
-        if len(argv) > 2:
-            BACKUP = open(argv[2], 'w')
+    tickets_creator = TicketCreate(URL)
+    tickets = tickets_creator.get_tickets()
 
-        Tickets(FILE, BACKUP).run()
+    with open(args.f, 'w') as f:
+        f.write(json.dumps(tickets, indent=2, ensure_ascii=False))
 
-        if FILE != stdout:
-            FILE.close()
-        if BACKUP != stderr:
-            BACKUP.close()
+
+
+if __name__ == '__main__':
+    main()
