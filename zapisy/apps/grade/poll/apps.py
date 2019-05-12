f627770de329824c399dede1db8b978de229def2
Marek Kochanowski <marek@kochanow.ski>
2019-05-12 18:44:10 +0200

    Remove legacy code from Poll app

--- zapisy/apps/grade/poll/apps.py
+++ zapisy/apps/grade/poll/apps.py
@@ -1,10 +1,5 @@
-"""This code exists only to enable signals."""
 from django.apps import AppConfig
 
-
-class PollLegacyAppConfig(AppConfig):
-    name = "apps.grade.poll"
-    verbose_name = "Poll - legacy"
 
 class PollAppConfig(AppConfig):
     name = "apps.grade.poll"
