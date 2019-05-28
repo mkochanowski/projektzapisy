624e7fbaaaf08485f606208228d532c298e167b3
Jan Marcinkowski <jasiekmarc@users.noreply.github.com>
2019-05-28 14:18:58 +0200

    Przepisanie aplikacji Preferences (#686)
    Aplikacja Preferences pomaga Dyrektorowi ds. Dydaktycznych przydzielić pracowników do prowadzenia zajęć. Dyrektor zaznacza przedmioty, których obsadzenie jest kłopotliwe jako w preferencjach, a następnie pracownicy wypełniają, które z tych zajęć mogą prowadzić, a których nie chcą.
    
    Pretekstem do zmian w tej aplikacji jest wprowadzenie nowego modelu propozycji przedmiotu (#669), ale przy okazji dokonaliśmy trochę czyszczenia.
    
    Największa zmiana polega na tym, że teraz wybór przedmiotów w preferencjach dokonuje się w aplikacji. Wcześniej miało to miejsce w podstawie przedmiotu.

--- zapisy/apps/offer/preferences/urls.py
+++ zapisy/apps/offer/preferences/urls.py
@@ -1,12 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
-# to view and description, pass by GET:
-# format=json - returns data as json
-# format=html - returns data rendered with template, not using the base template
-# by default, they return a fully rendered page
-
 urlpatterns = [
-    url(r'^$', views.view, name='prefs-default-view'),
-    url(r'^save/$', views.save, name='preference-save'),
+    path('', views.main, name='preferences-main')
 ]
--- zapisy/zapisy/pipeline.py
+++ zapisy/zapisy/pipeline.py
@@ -273,22 +273,4 @@ PIPELINE_JS = {
         ),
         'output_filename': 'js/studentlist.min.js',
     },
-
-    'preferences': {
-        'source_filenames': (
-            'js/jquery/jquery-1.5.2.js',
-            'js/jquery/jquery-ui-1.8.17.custom.js',
-            'js/jquery/jquery.cookies.2.2.0.min.js',
-            'js/jquery/baseExtensions.js',
-            'js/main.js',
-            'js/common/bootstrap-dropdown.js',
-            'js/components/messageBox.js',
-            'js/components/topBarFilter.js',
-            #'js/common/listFilter.js',
-            #'js/common/listFilter-courseType.js',
-            #'js/offer/proposal/proposals-list.js'
-        ),
-        'output_filename': 'js/preferenes.min.js',
-
-    }
 }
