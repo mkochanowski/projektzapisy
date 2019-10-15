6034c3659f2c657437e7ee71382df715a4052d11
Jasiek Busz <jasiekbusz@gmail.com>
2019-10-15 20:26:38 +0200

    Dodano opcję mailto dla pracowników do głosujących na propozycję (#783)
    W wynikach głosowania na propozycję przedmiotu pracownikom wyświetlają się teraz linki z mailto do głosujących na daną propozycję.
    
    Closes #771

--- zapisy/apps/enrollment/utils.py
+++ zapisy/apps/enrollment/utils.py
@@ -43,6 +43,7 @@ def send_mail(msg_parts, user):
 def mailto(author, students, bcc=False):
     """Helper method to create mailto links"""
     result = author.email
+
     if students:
         return result + ('?bcc=' if bcc else ',') + \
             ','.join([student.user.email.replace('+', '%2B') for student in students])
